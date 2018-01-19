f9db029002e851a9d662b5f0c8369a6397667ca6
Henson <henso@amazon.com>
2018-01-19 14:47:48 -0800

    Reworked clock stuff to use posix on newer darwin, and fallback to supported mechanisms on older darwin.

--- source/posix/clock.c
+++ source/posix/clock.c
@@ -15,28 +15,61 @@
 
 #include <aws/common/clock.h>
 #include <time.h>
+#if defined(__MACH__) && MAC_OS_X_VERSION_MAX_ALLOWED < 101200
+#include <sys/time.h>
+#endif /*defined(__MACH__) && MAC_OS_X_VERSION_MAX_ALLOWED < 101200*/
 
-static const long NS_PER_SEC = 1000000000;
+static const uint64_t NS_PER_SEC = 1000000000;
+
+#if defined(CLOCK_MONOTONIC_RAW)
+#define HIGH_RES_CLOCK CLOCK_MONOTONIC_RAW
+#else
+#define HIGH_RES_CLOCK CLOCK_MONOTONIC
+#endif
 
 int aws_high_res_clock_get_ticks(uint64_t *timestamp) {
-    struct timespec ts;
-    int ret_val = clock_gettime(CLOCK_MONOTONIC, &ts);
+    int ret_val = 0;
+
+#if defined(__MACH__) && MAC_OS_X_VERSION_MAX_ALLOWED < 101200
+    struct timeval tv;
+    ret_val = gettimeofday(&tv, NULL);
+
     if(ret_val) {
         return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
     }
 
+    *timestamp = tv.tv_sec * NS_PER_SEC + tv.tv_usec * 1000;
+#else
+    struct timespec ts;
+
+    ret_val = clock_gettime(HIGH_RES_CLOCK, &ts);
+
+    if(ret_val) {
+        return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
+
+    }
+
     *timestamp = (uint64_t)((ts.tv_sec * NS_PER_SEC) + ts.tv_nsec);
+#endif /*defined(__MACH__) && MAC_OS_X_VERSION_MAX_ALLOWED < 101200*/
+
     return AWS_OP_SUCCESS;
 }
 
 int aws_sys_clock_get_ticks(uint64_t *timestamp) {
+#if defined(__MACH__) && MAC_OS_X_VERSION_MAX_ALLOWED < 101200
+    return aws_high_res_clock_get_ticks(timestamp);
+#else
+    int ret_val = 0;
+
     struct timespec ts;
-    int ret_val = clock_gettime(CLOCK_REALTIME, &ts);
+    ret_val = clock_gettime(CLOCK_REALTIME, &ts);
     if(ret_val) {
         return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
     }
 
     *timestamp = (uint64_t)((ts.tv_sec * NS_PER_SEC) + ts.tv_nsec);
+#endif /*defined(__MACH__) && MAC_OS_X_VERSION_MAX_ALLOWED < 101200*/
+
     return AWS_OP_SUCCESS;
 }
 
