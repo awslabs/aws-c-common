dfac536135d2989d8971e938a3f88827b8459d2b
Jonathan M. Henson <henso@amazon.com>
2018-03-01 23:54:22 -0800

    Addition of string/byte buf type, updated the encoding interfaces and tests.

--- include/aws/common/byte_buf.h
+++ include/aws/common/byte_buf.h
@@ -19,20 +19,20 @@
 #include <string.h>
 
 struct aws_byte_buf {
-    const uint8_t *buffer;
+    uint8_t *buffer;
     size_t len;
 };
 
 static inline struct aws_byte_buf aws_byte_buf_from_literal(const char *literal) {
-    return {.buffer = (const uint8_t *)literal, .len = strlen(literal)};
+    return (struct aws_byte_buf){ .buffer = (uint8_t *)literal, .len = strlen(literal)};
 }
 
 static inline struct aws_byte_buf aws_byte_buf_from_c_str(const char *c_str, size_t len) {
-    return {.buffer = (const uint8_t *)c_str, .len = len};
+    return (struct aws_byte_buf){.buffer = (uint8_t *)c_str, .len = len};
 }
 
-static inline struct aws_byte_buf aws_byte_buf_from_array(const char c_str[], size_t len) {
-    return {.buffer = (const uint8_t *)&c_str[0], .len = len};
+static inline struct aws_byte_buf aws_byte_buf_from_array(const uint8_t c_str[], size_t len) {
+    return (struct aws_byte_buf){.buffer = (uint8_t *)&c_str[0], .len = len};
 }
 
 
