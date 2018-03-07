ff7591b43d208050fa34e065cc780d370ee81f38
Bryan Donlan <bdonlan@amazon.com>
2018-03-07 13:38:57 -0800

    Reformat comments in math.h

--- include/aws/common/math.h
+++ include/aws/common/math.h
@@ -24,21 +24,21 @@
  */
 static inline uint64_t aws_common_mul_u64_saturating(uint64_t a, uint64_t b) {
 #if defined(__x86_64__) && defined(__GNUC__)
-    // We can use inline assembly to do this efficiently on x86-64 and x86.
+    /* We can use inline assembly to do this efficiently on x86-64 and x86. */
 
-    // we specify rdx as an output, rather than a clobber, because we want to
-    // allow it to be allocated as an input register
+    /* we specify rdx as an output, rather than a clobber, because we want to */
+    /* allow it to be allocated as an input register */
 
     uint64_t rdx;
     __asm__(
-        "mulq %q[arg2]\n" // rax * b, result is in RDX:RAX, OF=CF=(RDX != 0)
+        "mulq %q[arg2]\n" /* rax * b, result is in RDX:RAX, OF=CF=(RDX != 0) */
         "cmovc %q[saturate], %%rax\n"
         : /* in/out: %rax = a, out: rdx (ignored) */ "+&a" (a), "=&d" (rdx)
         : /* in: anything (imm/reg/memory) */ [arg2] "g" (b),
           /* in: saturation value (reg/memory) */ [saturate] "rm" (~0LL)
         : /* clobbers: cc */ "cc"
     );
-    (void)rdx; // suppress unused warnings
+    (void)rdx; /* suppress unused warnings */
     return a;
 #else
     uint64_t x = a * b;
@@ -56,14 +56,14 @@ static inline uint64_t aws_common_mul_u64_saturating(uint64_t a, uint64_t b) {
  */
 static inline int aws_common_mul_u64_checked(uint64_t a, uint64_t b, uint64_t *r) {
 #if defined(__x86_64__) && defined(__GNUC__)
-    // We can use inline assembly to do this efficiently on x86-64 and x86.
+    /* We can use inline assembly to do this efficiently on x86-64 and x86. */
 
     int flag;
     uint64_t result = a;
     __asm__(
-        "mulq %q[arg2]\n" // rax * b, result is in RDX:RAX, OF=CF=(RDX != 0)
-        "setno %%dl\n" // rdx = (OF = 0)
-        "and $0xFF, %%edx\n" // mask out flag
+        "mulq %q[arg2]\n" /* rax * b, result is in RDX:RAX, OF=CF=(RDX != 0) */
+        "setno %%dl\n" /* rdx = (OF = 0) */
+        "and $0xFF, %%edx\n" /* mask out flag */
         : /* in/out: %rax (with first operand) */ "+&a" (result), "=&d" (flag)
         : /* in: imm/reg/memory for 2nd operand */
           [arg2] "g" (b)
@@ -87,17 +87,17 @@ static inline int aws_common_mul_u64_checked(uint64_t a, uint64_t b, uint64_t *r
  */
 static inline uint32_t aws_common_mul_u32_saturating(uint32_t a, uint32_t b) {
 #if (defined(__i386__) || defined(__x86_64__)) && defined(__GNUC__)
-    // We can use inline assembly to do this efficiently on x86-64 and x86.
+    /* We can use inline assembly to do this efficiently on x86-64 and x86. */
 
-    // we specify edx as an output, rather than a clobber, because we want to allow it
-    // to be allocated as an input register
+    /* we specify edx as an output, rather than a clobber, because we want to allow it */
+    /* to be allocated as an input register */
     uint32_t edx;
     __asm__(
-        "mull %k[arg2]\n" // eax * b, result is in EDX:EAX, OF=CF=(EDX != 0)
+        "mull %k[arg2]\n" /* eax * b, result is in EDX:EAX, OF=CF=(EDX != 0) */
 #ifdef __x86_64__
         "cmovc %k[saturate], %%eax\n"
 #else
-        // cmov isn't guaranteed to be available on x86-32
+        /* cmov isn't guaranteed to be available on x86-32 */
         "jno .1f\n"
         "mov $0xFFFFFFFF, %%eax\n"
         ".1f:"
@@ -105,12 +105,12 @@ static inline uint32_t aws_common_mul_u32_saturating(uint32_t a, uint32_t b) {
         : /* in/out: %rax = result/a, out: rdx (ignored) */ "+&a" (a), "=&d" (edx)
         : /* in: operand 2 (imm/reg/memory) */ [arg2] "g" (b)
 #ifdef __x86_64__
-        // Saturation value for CMOV to use
+        /* Saturation value for CMOV to use */
             , [saturate] "r" (0xFFFFFFFF)
 #endif
         : /* clobbers: cc */ "cc"
     );
-    (void)edx; // suppress unused warnings
+    (void)edx; /* suppress unused warnings */
     return a;
 #else
     uint32_t x = a * b;
@@ -128,17 +128,17 @@ static inline uint32_t aws_common_mul_u32_saturating(uint32_t a, uint32_t b) {
  */
 static inline int aws_common_mul_u32_checked(uint32_t a, uint32_t b, uint32_t *r) {
 #if (defined(__i386__) || defined(__x86_64__)) && defined(__GNUC__)
-    // We can use inline assembly to do this efficiently on x86-64 and x86.
+    /* We can use inline assembly to do this efficiently on x86-64 and x86. */
     uint32_t result = a;
     int flag;
-    // Note: We use SETNO which only takes a byte register. To make this easy,
-    // we'll write it to dl (which we throw away anyway) and mask off the high
-    // bits.
+    /* Note: We use SETNO which only takes a byte register. To make this easy, */
+    /* we'll write it to dl (which we throw away anyway) and mask off the high */
+    /* bits. */
     __asm__(
-        "mull %k[arg2]\n" // eax * b, result is in EDX:EAX, OF=CF=(EDX != 0)
-        "setno %%dl\n" // flag = !OF ^ (junk in top 24 bits)
-        "and $0xFF, %%edx\n" // flag = flag & 0xFF
-        // we allocate flag to EDX since it'll be clobbered by MUL anyway
+        "mull %k[arg2]\n" /* eax * b, result is in EDX:EAX, OF=CF=(EDX != 0) */
+        "setno %%dl\n" /* flag = !OF ^ (junk in top 24 bits) */
+        "and $0xFF, %%edx\n" /* flag = flag & 0xFF */
+        /* we allocate flag to EDX since it'll be clobbered by MUL anyway */
         : /* in/out: %eax = a, %dl = flag */ "+&a" (result), "=&d" (flag)
         : /* imm/reg/memory = b */ [arg2] "g" (b)
         : /* clobbers: cc */ "cc"
@@ -158,13 +158,13 @@ static inline int aws_common_mul_u32_checked(uint32_t a, uint32_t b, uint32_t *r
 
 
 static inline size_t aws_common_mul_size_saturating(size_t a, size_t b) {
-    // static assert: SIZE_MAX == (~(uint32_t)0) || (~(uint64_t)0)
+    /* static assert: SIZE_MAX == (~(uint32_t)0) || (~(uint64_t)0) */
     char assert_sizet_is_32_or_64_bit[
         (((uint64_t)SIZE_MAX == (uint64_t)~(uint32_t)0) ||
         ((uint64_t)SIZE_MAX == (uint64_t)~(uint64_t)0))
         ? 1 : -1
     ];
-    // suppress unused variable warning
+    /* suppress unused variable warning */
     (void)assert_sizet_is_32_or_64_bit;
 
     if ((uint64_t)SIZE_MAX == (uint64_t)~(uint32_t)0) {
