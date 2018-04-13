8c52c0adce43eeeb5ddd839ef9da2f5306e632a2
Jonathan M. Henson <henso@amazon.com>
2018-04-12 17:08:12 -0700

    Initial work for condition variables, implemented posix.

---
+++ include/aws/common/condition_variable.h
@@ -0,0 +1,71 @@
+#ifndef AWS_CONDITION_VARIABLE_H
+#define AWS_CONDITION_VARIABLE_H
+
+/*
+* Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+#include <aws/common/common.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <pthread.h>
+#endif
+
+struct aws_mutex;
+
+struct aws_condition_variable {
+#ifdef _WIN32
+    CONDITION_VARIABLE condition_handle;
+#else
+    pthread_cond_t condition_handle;
+#endif
+};
+
+#ifdef _WIN32
+#define AWS_CONDITION_VARIABLE_INIT                                                                                    \
+{ .condition_handle = CONDITION_VARIABLE_INIT }
+#else
+#define AWS_MUTEX_INIT                                                                                                 \
+{ .condition_handle = PTHREAD_COND_INITIALIZER }
+#endif
+
+typedef bool(aws_condition_variable_predicate)(struct aws_condition_variable *condition_variable, void *ctx);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+AWS_COMMON_API int aws_condition_variable_init (struct aws_condition_variable *condition_variable);
+
+AWS_COMMON_API void aws_condition_variable_clean_up (struct aws_condition_variable *condition_variable);
+
+AWS_COMMON_API int aws_condition_variable_notify_one (struct aws_condition_variable *condition_variable);
+
+AWS_COMMON_API int aws_condition_variable_notify_all (struct aws_condition_variable *condition_variable);
+
+AWS_COMMON_API int aws_condition_variable_wait (struct aws_condition_variable *condition_variable, struct aws_mutex *mutex);
+
+AWS_COMMON_API int aws_condition_variable_wait_for (struct aws_condition_variable *condition_variable, struct aws_mutex *mutex,
+                                                    aws_condition_variable_predicate predicate, void *ctx);
+
+
+AWS_COMMON_API int aws_condition_variable_wait_until (struct aws_condition_variable *condition_variable, struct aws_mutex *mutex,
+                                                    uint64_t time_to_wait);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*AWS_CONDITION_VARIABLE_H */
---
+++ source/posix/condition_variable.c
@@ -0,0 +1,90 @@
+/*
+* Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+#include <aws/common/condition_variable.h>
+#include <aws/common/mutex.h>
+
+#include <errno.h>
+
+#define NANOS_PER_SEC 1000000000
+
+static int process_error_code (int err) {
+    switch (err) {
+        case ENOMEM:
+            return aws_raise_error(AWS_ERROR_OOM);
+        case ETIMEDOUT:
+            return aws_raise_error(AWS_ERROR_COND_VARIABLE_TIMED_OUT);
+        default:
+            return aws_raise_error(AWS_ERROR_COND_VARIABLE_ERROR_UNKNOWN);
+    }
+}
+
+int aws_condition_variable_init (struct aws_condition_variable *condition_variable) {
+    if (pthread_cond_init(&condition_variable->condition_handle, NULL) ) {
+        return aws_raise_error(AWS_ERROR_COND_VARIABLE_INIT_FAILED);
+    }
+
+    return AWS_OP_SUCCESS;
+}
+
+void aws_condition_variable_clean_up (struct aws_condition_variable *condition_variable) {
+    pthread_cond_destroy(&condition_variable->condition_handle);
+}
+
+int aws_condition_variable_notify_one (struct aws_condition_variable *condition_variable) {
+    int err_code = pthread_cond_signal(&condition_variable->condition_handle);
+
+    if (err_code) {
+        return process_error_code(err_code);
+    }
+
+    return AWS_OP_SUCCESS;
+}
+
+int aws_condition_variable_notify_all (struct aws_condition_variable *condition_variable) {
+    int err_code = pthread_cond_broadcast(&condition_variable->condition_handle);
+
+    if (err_code) {
+        return process_error_code(err_code);
+    }
+
+    return AWS_OP_SUCCESS;
+}
+
+int aws_condition_variable_wait (struct aws_condition_variable *condition_variable, struct aws_mutex *mutex) {
+    int err_code = pthread_cond_wait(&condition_variable->condition_handle, &mutex->mutex_handle);
+
+    if (err_code) {
+        return process_error_code(err_code);
+    }
+
+    return AWS_OP_SUCCESS;
+}
+
+int aws_condition_variable_wait_until (struct aws_condition_variable *condition_variable, struct aws_mutex *mutex,
+                                                      uint64_t time_to_wait) {
+
+    struct timespec ts;
+    ts.tv_sec = time_to_wait / NANOS_PER_SEC;
+    ts.tv_nsec = time_to_wait % NANOS_PER_SEC;
+
+    int err_code = pthread_cond_timedwait(&condition_variable->condition_handle, &mutex->mutex_handle, &ts);
+
+    if (err_code) {
+        return process_error_code(err_code);
+    }
+
+    return AWS_OP_SUCCESS;
+}
