f61c19441e8bb266700aaed5e95ec6c760a25424
Colden Cullen <ColdenCullen@Gmail.com>
2018-06-25 10:53:13 -0700

    Clang tidy fixes (#70)
    * Add minimal .clang-tidy file
    
    * Fix clang-tidy build
    
    - Disable valist.Uninitialized, it triggers a false positive.
    - Add asserts to aws_array_list_mem_swap to validate parameters.
    - task_schedule.c: Add asserts to validate task functions before call.
        This is required because the analyzer doesn't know that
        aws_raise_error() always returns 1, so it can't make
        branch flow assumptions.
    
    * Enable clang-tidy in linux-clang codebuild
    
    * Turns out you can just install clang-tidy.
    
    * clang-tidy-3.9 doesn't like FormatStyle: none, removing

--- source/task_scheduler.c
+++ source/task_scheduler.c
@@ -17,6 +17,8 @@
 #include <aws/common/thread.h>
 #include <aws/common/priority_queue.h>
 
+#include <assert.h>
+
 static const size_t DEFAULT_QUEUE_SIZE = 7;
 
 struct task_container {
@@ -33,7 +35,7 @@ static int compare_timestamps(const void *a, const void *b) {
 static inline int get_next_task(struct aws_task_scheduler *scheduler, struct aws_task *task, uint64_t run_before,
                                 uint64_t *next_run_time);
 
-int aws_task_scheduler_init(struct aws_task_scheduler *scheduler, struct aws_allocator *alloc, 
+int aws_task_scheduler_init(struct aws_task_scheduler *scheduler, struct aws_allocator *alloc,
         aws_task_scheduler_clock clock) {
     scheduler->alloc = alloc;
     scheduler->clock = clock;
@@ -52,6 +54,7 @@ void aws_task_scheduler_clean_up(struct aws_task_scheduler *scheduler) {
             break;
         }
 
+        assert(task_to_run.fn);
         task_to_run.fn(task_to_run.arg, AWS_TASK_STATUS_CANCELED);
     }
 
@@ -97,7 +100,7 @@ static inline int get_next_task(struct aws_task_scheduler *scheduler, struct aws
     return AWS_OP_SUCCESS;
 }
 
-int aws_task_scheduler_next_task(struct aws_task_scheduler *scheduler, 
+int aws_task_scheduler_next_task(struct aws_task_scheduler *scheduler,
         struct aws_task *task, uint64_t *next_run_time) {
 
     uint64_t now;
@@ -119,7 +122,7 @@ int aws_task_scheduler_schedule_now(struct aws_task_scheduler *scheduler,
     return aws_task_scheduler_schedule_future(scheduler, task, now);
 }
 
-int aws_task_scheduler_schedule_future(struct aws_task_scheduler *scheduler, 
+int aws_task_scheduler_schedule_future(struct aws_task_scheduler *scheduler,
         struct aws_task *task, uint64_t time_to_run) {
 
     struct task_container container;
@@ -168,6 +171,7 @@ int aws_task_scheduler_run_all(struct aws_task_scheduler *scheduler, uint64_t *n
             return AWS_OP_ERR;
         }
 
+        assert(task_to_run.fn);
         task_to_run.fn(task_to_run.arg, AWS_TASK_STATUS_RUN_READY);
     }
 }
