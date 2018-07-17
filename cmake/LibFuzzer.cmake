0c5656ecc728a71e1e482bb2501504f9fadcf5e5
Colden Cullen <ColdenCullen@Gmail.com>
2018-07-17 16:12:42 -0700

    LibFuzzer Helper (#84)
    - Add LibFuzzer helper library
    - Add fuzz tests for hex & base64 encoding
    - Add linux clang6 build

---
+++ cmake/LibFuzzer.cmake
@@ -0,0 +1,45 @@
+# Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://aws.amazon.com/apache2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+include(CTest)
+include(Sanitizers)
+
+option(ENABLE_FUZZ_TESTS "Build and run fuzz tests" OFF)
+set(FUZZ_TESTS_MAX_TIME 60 CACHE STRING "Max time to run each fuzz test")
+
+# Adds fuzz tests to ctest
+# Options:
+#  fuzz_files: The list of fuzz test files
+#  other_files: Other files to link into each fuzz test
+function(aws_add_fuzz_tests fuzz_files other_files)
+    if(ENABLE_FUZZ_TESTS)
+        aws_check_sanitizer(fuzzer)
+        if (NOT HAS_SANITIZER_fuzzer)
+            message(FATAL_ERROR "ENABLE_FUZZ_TESTS is set but the current compiler (${CMAKE_CXX_COMPILER_ID}) doesn't support -fsanitize=fuzzer")
+        endif()
+
+        foreach(test_file ${fuzz_files})
+            get_filename_component(TEST_FILE_NAME ${test_file} NAME_WE)
+
+            set(FUZZ_BINARY_NAME ${CMAKE_PROJECT_NAME}-fuzz-${TEST_FILE_NAME})
+            add_executable(${FUZZ_BINARY_NAME} ${test_file} ${other_files})
+            target_link_libraries(${FUZZ_BINARY_NAME} ${CMAKE_PROJECT_NAME})
+            aws_set_common_properties(${FUZZ_BINARY_NAME})
+            aws_add_sanitizers(${FUZZ_BINARY_NAME} SANITIZERS "${${CMAKE_PROJECT_NAME}_SANITIZERS};fuzzer")
+            target_compile_definitions(${FUZZ_BINARY_NAME} PRIVATE AWS_UNSTABLE_TESTING_API=1)
+            target_include_directories(${FUZZ_BINARY_NAME} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
+
+            add_test(NAME fuzz_${TEST_FILE_NAME} COMMAND ${FUZZ_BINARY_NAME} -timeout=1 -max_total_time=${FUZZ_TESTS_MAX_TIME})
+        endforeach()
+    endif()
+endfunction()
---
+++ tests/fuzz/base64_encoding_transitive.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/common/encoding.h>
+
+#include <assert.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+
+    struct aws_allocator *alloc = aws_default_allocator();
+
+    size_t output_size = 0;
+    int result = aws_base64_compute_encoded_len(size, &output_size);
+    assert(result == AWS_OP_SUCCESS);
+
+    struct aws_byte_buf to_encode = aws_byte_buf_from_array(data, size);
+
+    struct aws_byte_buf encode_output;
+    result = aws_byte_buf_init(alloc, &encode_output, output_size);
+    assert(result == AWS_OP_SUCCESS);
+
+    result = aws_base64_encode(&to_encode, &encode_output);
+    assert(result == AWS_OP_SUCCESS);
+    --encode_output.len; /* Remove null terminator */
+
+    result = aws_base64_compute_decoded_len((const char *)encode_output.buffer, encode_output.len, &output_size);
+    assert(result == AWS_OP_SUCCESS);
+    assert(output_size == size);
+
+    struct aws_byte_buf decode_output;
+    result = aws_byte_buf_init(alloc, &decode_output, output_size);
+    assert(result == AWS_OP_SUCCESS);
+
+    result = aws_base64_decode(&encode_output, &decode_output);
+    assert(result == AWS_OP_SUCCESS);
+    assert(output_size == decode_output.len);
+    assert(memcmp(decode_output.buffer, data, size) == 0);
+
+    aws_byte_buf_clean_up(&encode_output);
+    aws_byte_buf_clean_up(&decode_output);
+
+    return 0;
+}
