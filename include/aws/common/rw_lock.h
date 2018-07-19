2d39b6bd603f03010a290c7f841aebd072102439
Colden Cullen <colden@amazon.com>
2018-07-18 17:47:10 -0700

    aws_rw_lock functions should be inline, not export

--- include/aws/common/rw_lock.h
+++ include/aws/common/rw_lock.h
@@ -44,33 +44,33 @@ extern "C" {
 /**
  * Initializes a new platform instance of mutex.
  */
-AWS_COMMON_API int aws_rw_lock_init(struct aws_rw_lock *lock);
+inline int aws_rw_lock_init(struct aws_rw_lock *lock);
 
 /**
  * Cleans up internal resources.
  */
-AWS_COMMON_API void aws_rw_lock_clean_up(struct aws_rw_lock *lock);
+inline void aws_rw_lock_clean_up(struct aws_rw_lock *lock);
 
 /**
  * Blocks until it acquires the lock. While on some platforms such as Windows, this may behave as a reentrant mutex,
  * you should not treat it like one. On platforms it is possible for it to be non-reentrant, it will be.
  */
-AWS_COMMON_API int aws_rw_lock_rlock(struct aws_rw_lock *lock);
-AWS_COMMON_API int aws_rw_lock_wlock(struct aws_rw_lock *lock);
+inline int aws_rw_lock_rlock(struct aws_rw_lock *lock);
+inline int aws_rw_lock_wlock(struct aws_rw_lock *lock);
 
 /**
  * Attempts to acquire the lock but returns immediately if it can not.
  * While on some platforms such as Windows, this may behave as a reentrant mutex,
  * you should not treat it like one. On platforms it is possible for it to be non-reentrant, it will be.
 */
-AWS_COMMON_API int aws_rw_lock_try_rlock(struct aws_rw_lock *lock);
-AWS_COMMON_API int aws_rw_lock_try_wlock(struct aws_rw_lock *lock);
+inline int aws_rw_lock_try_rlock(struct aws_rw_lock *lock);
+inline int aws_rw_lock_try_wlock(struct aws_rw_lock *lock);
 
 /**
  * Releases the lock.
  */
-AWS_COMMON_API int aws_rw_lock_runlock(struct aws_rw_lock *lock);
-AWS_COMMON_API int aws_rw_lock_wunlock(struct aws_rw_lock *lock);
+inline int aws_rw_lock_runlock(struct aws_rw_lock *lock);
+inline int aws_rw_lock_wunlock(struct aws_rw_lock *lock);
 
 #ifdef __cplusplus
 }
