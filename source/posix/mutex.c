24fe76ece1427ff5c294a40be1e4901772f8aed9
Jonathan M. Henson <henso@amazon.com>
2018-09-25 16:45:24 -0700

    added newline back.

--- source/posix/mutex.c
+++ source/posix/mutex.c
@@ -12,6 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 #include <aws/common/mutex.h>
 
 #include <errno.h>
