9183c6d4bd27665c06d22283a75102010ce78999
Jonathan M. Henson <jonathan.michael.henson@gmail.com>
2018-12-12 11:17:06 -0800

    Fixed header include. (#210)

--- source/posix/device_random.c
+++ source/posix/device_random.c
@@ -12,6 +12,8 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+#include <aws/common/device_random.h>
+
 #include <aws/common/byte_buf.h>
 #include <aws/common/thread.h>
 
