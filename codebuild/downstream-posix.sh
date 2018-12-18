c865372b4b064a4704f86ba8348001a89c3a2f80
Justin Boswell <boswej@amazon.com>
2018-12-18 15:14:39 -0800

    s2n does not need tests, now building the current PR for aws-c-common instead of head

--- codebuild/downstream-posix.sh
+++ codebuild/downstream-posix.sh
@@ -17,37 +17,38 @@ install_prefix=$build_dir/install
 
 cmake_args=""
 
-function build_project {
-    local dep=$1
-    local commit_or_branch=$2
-
-    # if the local deps are preferred and the local dep exists, use it
-    if [ $prefer_local_deps -ne 0 ] && [ -d "$home_dir/../$dep" ]; then
-        pushd $home_dir/../$dep
-        echo "Using local repo: $home_dir/../$dep branch:" `git branch | grep \* | cut -d ' ' -f2` "commit: " `git rev-parse HEAD`
-    else # git clone the repo and build it
-        pushd $build_dir
-        if [ -d $dep ]; then
-            cd $dep
-            git pull --rebase
-        else
-            git clone https://github.com/awslabs/$dep.git
-            cd $dep
-        fi
-
-        if [ -n "$commit_or_branch" ]; then
-            git checkout $commit_or_branch
-        fi
-        echo "Using git repo: $dep branch:" `git branch | grep \* | cut -d ' ' -f2` "commit: " `git rev-parse HEAD`
-    fi
-
-    mkdir -p dep-build
-    cd dep-build
-
+function cmake_project {
+    local $proj_dir=$1
+    pushd $proj_dir
+    mkdir -p ci-build
+    cd ci-build
     cmake -G"Unix Makefiles" $cmake_args -DCMAKE_INSTALL_PREFIX=$install_prefix ..
     cmake --build . --target all
     cmake --build . --target install
     cmake --build . --target test
+    popd
+}
+
+function build_project {
+    local dep=$1
+    local commit_or_branch=$2
+
+    # git clone the repo and build it
+    pushd $build_dir
+    if [ -d $dep ]; then
+        cd $dep
+        git pull --rebase
+    else
+        git clone https://github.com/awslabs/$dep.git
+        cd $dep
+    fi
+
+    if [ -n "$commit_or_branch" ]; then
+        git checkout $commit_or_branch
+    fi
+    echo "Using git repo: $dep branch:" `git branch | grep \* | cut -d ' ' -f2` "commit: " `git rev-parse HEAD`
+
+    cmake_project $dep
 
     popd
 }
@@ -74,11 +75,19 @@ if [ $clean ]; then
 fi
 mkdir -p $build_dir
 
-build_project aws-c-common
+# build s2n without tests
+if [ "$(uname)" != "Darwin"]; then
+    default_cmake_args=$cmake_args
+    cmake_args="$cmake_args -DBUILD_TESTING=OFF"
+    build_project s2n
+    cmake_args=$default_cmake_args
+fi
+
+# build aws-c-common from this pr/branch
+cmake_project ..
+
+# build master head rev of downstream projects
 build_project aws-checksums
 build_project aws-c-event-stream
-if [ "$(uname)" != "Darwin"]; then
-    build_project s2n
-fi
 build_project aws-c-io
 build_project aws-c-mqtt
