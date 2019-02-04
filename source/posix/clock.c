c43527365884028d545db0b1376e5787b15a77d6
Jonathan M. Henson <henso@amazon.com>
2019-02-04 11:25:32 -0800

    Ran formatter.

--- source/posix/clock.c
+++ source/posix/clock.c
@@ -25,82 +25,83 @@ static const uint64_t NS_PER_SEC = 1000000000;
 #    define HIGH_RES_CLOCK CLOCK_MONOTONIC
 #endif
 
-/* This entire compilation branch has two goals. First, prior to OSX Sierra, clock_gettime does not exist on OSX, so we already
- * need to branch on that. Second, even if we compile on a newer OSX, which we will always do for bindings (e.g. python, dotnet, java etc...),
- * we have to worry about the same lib being loaded on an older version, and thus, we'd get linker errors at runtime. To avoid this, we do a dynamic load
- * to keep the function out of linker tables and only use the symbol if the current running process has access to the function. */
+/* This entire compilation branch has two goals. First, prior to OSX Sierra, clock_gettime does not exist on OSX, so we
+ * already need to branch on that. Second, even if we compile on a newer OSX, which we will always do for bindings (e.g.
+ * python, dotnet, java etc...), we have to worry about the same lib being loaded on an older version, and thus, we'd
+ * get linker errors at runtime. To avoid this, we do a dynamic load
+ * to keep the function out of linker tables and only use the symbol if the current running process has access to the
+ * function. */
 #if defined(__MACH__)
 #    include <AvailabilityMacros.h>
 #    include <dlfcn.h>
 #    include <sys/time.h>
 
-     static int s_legacy_get_time(uint64_t *timestamp) {
-         struct timeval tv;
-         int ret_val = gettimeofday(&tv, NULL);
+static int s_legacy_get_time(uint64_t *timestamp) {
+    struct timeval tv;
+    int ret_val = gettimeofday(&tv, NULL);
 
-         if (ret_val) {
-             return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
-         }
+    if (ret_val) {
+        return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
+    }
 
-         *timestamp = tv.tv_sec * NS_PER_SEC + tv.tv_usec * 1000;
-         return AWS_OP_SUCCESS;
-     }
+    *timestamp = tv.tv_sec * NS_PER_SEC + tv.tv_usec * 1000;
+    return AWS_OP_SUCCESS;
+}
 
 #    if MAC_OS_X_VERSION_MAX_ALLOWED >= 101200
-         static bool s_gettime_init = false;
-         static int (*s_gettime_fn)(clockid_t __clock_id, struct timespec *__tp) = NULL;
+static bool s_gettime_init = false;
+static int (*s_gettime_fn)(clockid_t __clock_id, struct timespec *__tp) = NULL;
 
-         static void s_do_osx_loads() {
-            if (AWS_UNLIKELY(!s_gettime_init)) {
-                s_gettime_fn = (int(*)(clockid_t __clock_id, struct timespec *__tp))dlsym(RTLD_DEFAULT, "clock_gettime");
-                s_gettime_init = true;
-            }
-         }
+static void s_do_osx_loads() {
+    if (AWS_UNLIKELY(!s_gettime_init)) {
+        s_gettime_fn = (int (*)(clockid_t __clock_id, struct timespec * __tp)) dlsym(RTLD_DEFAULT, "clock_gettime");
+        s_gettime_init = true;
+    }
+}
 
-         int aws_high_res_clock_get_ticks(uint64_t *timestamp) {
-             s_do_osx_loads();
-             int ret_val = 0;
+int aws_high_res_clock_get_ticks(uint64_t *timestamp) {
+    s_do_osx_loads();
+    int ret_val = 0;
 
-             if (s_gettime_fn) {
-                 struct timespec ts;
-                 ret_val = s_gettime_fn(HIGH_RES_CLOCK, &ts);
+    if (s_gettime_fn) {
+        struct timespec ts;
+        ret_val = s_gettime_fn(HIGH_RES_CLOCK, &ts);
 
-                 if (ret_val) {
-                     return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
-                 }
+        if (ret_val) {
+            return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
+        }
 
-                 *timestamp = (uint64_t) ((ts.tv_sec * NS_PER_SEC) + ts.tv_nsec);
-                 return AWS_OP_SUCCESS;
-             }
+        *timestamp = (uint64_t)((ts.tv_sec * NS_PER_SEC) + ts.tv_nsec);
+        return AWS_OP_SUCCESS;
+    }
 
-             return s_legacy_get_time(timestamp);
-         }
+    return s_legacy_get_time(timestamp);
+}
 
-         int aws_sys_clock_get_ticks(uint64_t *timestamp) {
-             s_do_osx_loads();
-             int ret_val = 0;
+int aws_sys_clock_get_ticks(uint64_t *timestamp) {
+    s_do_osx_loads();
+    int ret_val = 0;
 
-             if (s_gettime_fn) {
-                 struct timespec ts;
-                 ret_val = s_gettime_fn(CLOCK_REALTIME, &ts);
-                 if (ret_val)
-                 {
-                     return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
-                 }
+    if (s_gettime_fn) {
+        struct timespec ts;
+        ret_val = s_gettime_fn(CLOCK_REALTIME, &ts);
+        if (ret_val) {
+            return aws_raise_error(AWS_ERROR_CLOCK_FAILURE);
+        }
 
-                 *timestamp = (uint64_t) ((ts.tv_sec * NS_PER_SEC) + ts.tv_nsec);
-                 return AWS_OP_SUCCESS;
-             }
-             return s_legacy_get_time(timestamp);
-         }
+        *timestamp = (uint64_t)((ts.tv_sec * NS_PER_SEC) + ts.tv_nsec);
+        return AWS_OP_SUCCESS;
+    }
+    return s_legacy_get_time(timestamp);
+}
 #    else
-         int aws_high_res_clock_get_ticks(uint64_t *timestamp) {
-             return s_legacy_get_time(timestamp);
-         }
+int aws_high_res_clock_get_ticks(uint64_t *timestamp) {
+    return s_legacy_get_time(timestamp);
+}
 
-         int aws_sys_clock_get_ticks(uint64_t *timestamp) {
-             return s_legacy_get_time(timestamp);
-         }
+int aws_sys_clock_get_ticks(uint64_t *timestamp) {
+    return s_legacy_get_time(timestamp);
+}
 
 #    endif /* MAC_OS_X_VERSION_MAX_ALLOWED >= 101200 */
 /* Everywhere else, just link clock_gettime in directly */
