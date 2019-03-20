cd2320d6277673528efc9046127485ef525fecb2
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-03-20 14:37:01 -0400

    use a simple version of memset

--- .cbmc-batch/stubs/memset_override.c
+++ .cbmc-batch/stubs/memset_override.c
@@ -13,90 +13,27 @@
  * permissions and limitations under the License.
  */
 
-/* FUNCTION: memset */
-
-#ifndef __CPROVER_STRING_H_INCLUDED
-#    include <string.h>
-#    define __CPROVER_STRING_H_INCLUDED
-#endif
-
-#undef memset
+#include <proof_helpers/nondet.h>
+#include <stddef.h>
+#define NONDET_THIS_MANY_BYTES 8
+/**
+ * Override the version of memset used by CBMC.
+ */
+void *memset_impl(void *s, int c, size_t n) {
+    __CPROVER_precondition(__CPROVER_w_ok(s, n), "memset destination region writeable");
+    if (n > 0) {
+        char *sp = (char *)s;
+        for (__CPROVER_size_t i = 0; i < n; i++)
+            sp[i] = c;
+    }
+    return s;
+}
 
 void *memset(void *s, int c, size_t n) {
-__CPROVER_HIDE:;
-#ifdef __CPROVER_STRING_ABSTRACTION
-    __CPROVER_precondition(__CPROVER_buffer_size(s) >= n, "memset buffer overflow");
-    if (__CPROVER_is_zero_string(s) && n > __CPROVER_zero_string_length(s)) {
-        __CPROVER_is_zero_string(s) = 1;
-    } else if (c == 0) {
-        __CPROVER_is_zero_string(s) = 1;
-        __CPROVER_zero_string_length(s) = 0;
-    } else
-        __CPROVER_is_zero_string(s) = 0;
-#else
-
-    if (n > 0) {
-        (void)*(char *)s;             /* check that the memory is accessible */
-        (void)*(((char *)s) + n - 1); /* check that the memory is accessible */
-        char *sp = s;
-        for (__CPROVER_size_t i = 0; i < n; i++)
-            sp[i] = c;
-    }
-#endif
-    return s;
+    return memset_impl(s, c, n);
 }
 
-/* FUNCTION: __builtin_memset */
-
-void *__builtin_memset(void *s, int c, __CPROVER_size_t n) {
-__CPROVER_HIDE:;
-#ifdef __CPROVER_STRING_ABSTRACTION
-    __CPROVER_precondition(__CPROVER_buffer_size(s) >= n, "memset buffer overflow");
-    if (__CPROVER_is_zero_string(s) && n > __CPROVER_zero_string_length(s)) {
-        __CPROVER_is_zero_string(s) = 1;
-    } else if (c == 0) {
-        __CPROVER_is_zero_string(s) = 1;
-        __CPROVER_zero_string_length(s) = 0;
-    } else {
-        __CPROVER_is_zero_string(s) = 0;
-    }
-#else
-
-    if (n > 0) {
-        (void)*(char *)s;             /* check that the memory is accessible */
-        (void)*(((char *)s) + n - 1); /* check that the memory is accessible */
-        char *sp = s;
-        for (__CPROVER_size_t i = 0; i < n; i++)
-            sp[i] = c;
-    }
-#endif
-    return s;
-}
-
-/* FUNCTION: __builtin___memset_chk */
-
-void *__builtin___memset_chk(void *s, int c, __CPROVER_size_t n, __CPROVER_size_t size) {
-__CPROVER_HIDE:;
-#ifdef __CPROVER_STRING_ABSTRACTION
-    __CPROVER_precondition(__CPROVER_buffer_size(s) >= n, "memset buffer overflow");
-    __CPROVER_precondition(__CPROVER_buffer_size(s) == size, "builtin object size");
-    if (__CPROVER_is_zero_string(s) && n > __CPROVER_zero_string_length(s)) {
-        __CPROVER_is_zero_string(s) = 1;
-    } else if (c == 0) {
-        __CPROVER_is_zero_string(s) = 1;
-        __CPROVER_zero_string_length(s) = 0;
-    } else
-        __CPROVER_is_zero_string(s) = 0;
-#else
-    (void)size;
-
-    if (n > 0) {
-        (void)*(char *)s;             /* check that the memory is accessible */
-        (void)*(((char *)s) + n - 1); /* check that the memory is accessible */
-        char *sp = s;
-        for (__CPROVER_size_t i = 0; i < n; i++)
-            sp[i] = c;
-    }
-#endif
-    return s;
+void *__builtin___memset_chk(void *s, int c, size_t n, size_t os) {
+    (void)os;
+    return memset_impl(s, c, n);
 }
