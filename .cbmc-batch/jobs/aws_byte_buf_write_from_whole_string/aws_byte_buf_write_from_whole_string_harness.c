00308920caf8bd8130fb76e386585c0577e3f98a
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-03-21 17:00:24 -0400

    fix typos in aws_byte_buf_write_from_whole_string

--- .cbmc-batch/jobs/aws_byte_buf_write_from_whole_string/aws_byte_buf_write_from_whole_string_harness.c
+++ .cbmc-batch/jobs/aws_byte_buf_write_from_whole_string/aws_byte_buf_write_from_whole_string_harness.c
@@ -22,7 +22,7 @@
 const size_t MAX_STRING_LEN = 16;
 
 /**
- * Coverage: 1.00 (69 lines out of 69 statically-reachable lines in 14 functions reached)
+ * Coverage:1.00 (70 lines out of 70 statically-reachable lines in 14 functions reached)
  * Runtime: real	0m7.394s
  */
 
@@ -30,11 +30,11 @@ void aws_byte_buf_write_from_whole_string_harness() {
     struct aws_byte_buf *buf = allocate_arbitrary_byte_buf_nondet_len_max(can_fail_allocator(), MAX_STRING_LEN);
     struct aws_byte_buf old_buf = *buf;
     // nondeterministially pick a btye. We can then track if it has changed
-    size_t index_to_check = Nondet_size_t();
+    size_t index_to_check = nondet_size_t();
     __CPROVER_assume(index_to_check < buf->len);
     uint8_t byte_old = buf->buffer[index_to_check];
     size_t availabie_cap = buf->capacity - buf->len;
-    struct aws_string *str = make_arbitrary_aws_string_nondet_len_with_max(Can_fail_allocator(), MAX_STRING_LEN);
+    struct aws_string *str = make_arbitrary_aws_string_nondet_len_with_max(can_fail_allocator(), MAX_STRING_LEN);
 
     bool rval = aws_byte_buf_write_from_whole_string(buf, str);
     // The value is appended to the end of the byte buf.  Make sure that worked.
