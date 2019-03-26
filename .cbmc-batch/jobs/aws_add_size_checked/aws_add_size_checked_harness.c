8b060f642f017d067cccfc10b569cb9647718ee6
Felipe R. Monteiro <rms.felipe@gmail.com>
2019-03-26 15:00:38 -0700

    Update harnesses for math.fallback.inl (#286)
    * Update harnesses for math.fallback.inl
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>
    
    * Replace nondet_int() with nondet_bool() for clarity
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>
    
    * Update list of assumptions
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>
    
    * Braces around if statements
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>
    
    * Add extra check in case checked functions fail
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/jobs/aws_add_size_checked/aws_add_size_checked_harness.c
+++ .cbmc-batch/jobs/aws_add_size_checked/aws_add_size_checked_harness.c
@@ -2,48 +2,34 @@
 #include <proof_helpers/make_common_data_structures.h>
 
 /**
- * Adds a + b and returns the result in *r. If the result
- * overflows, returns AWS_OP_ERR; otherwise returns AWS_OP_SUCCESS.
- */
-AWS_STATIC_IMPL int __aws_add_u64_checked(uint64_t a, uint64_t b, uint64_t *r) {
-    uint64_t x = a + b;
-    *r = x;
-    if (x < a) {
-        return aws_raise_error(AWS_ERROR_OVERFLOW_DETECTED);
-    }
-    return AWS_OP_SUCCESS;
-}
-
-/**
- * Adds a + b and returns the result in *r. If the result
- * overflows, returns AWS_OP_ERR; otherwise returns AWS_OP_SUCCESS.
- */
-AWS_STATIC_IMPL int __aws_add_u32_checked(uint32_t a, uint32_t b, uint32_t *r) {
-    uint32_t x = a + b;
-    *r = x;
-    if (x < a) {
-        return aws_raise_error(AWS_ERROR_OVERFLOW_DETECTED);
-    }
-    return AWS_OP_SUCCESS;
-}
-
-/**
+ * Coverage: 1.00 (31 lines out of 31 statically-reachable lines in 5 functions reached)
  * Runtime: 0m2.025s
+ *
+ * Assumptions:
+ *     - given 2 non-deterministics unsigned integers
+ *
+ * Assertions:
+ *     - r does not overflow, if aws_add_u32_checked or
+ *       aws_add_u64_checked functions return AWS_OP_SUCCESS
  */
 void aws_add_size_checked_harness() {
-    size_t r = nondet_size_t();
-    size_t __r = nondet_size_t();
-    __CPROVER_assume(r == __r);
-
-    if (nondet_int()) {
+    if (nondet_bool()) {
         uint64_t a = nondet_uint64_t();
         uint64_t b = nondet_uint64_t();
-        if (aws_add_u64_checked(a, b, &r) == AWS_OP_SUCCESS && __aws_add_u64_checked(a, b, &__r) == AWS_OP_SUCCESS)
-            assert(r == __r);
+        uint64_t r = nondet_uint64_t();
+        if (!aws_add_u64_checked(a, b, &r)) {
+            assert(r == a + b);
+        } else {
+            assert((b > 0) && (a > (UINT64_MAX - b)));
+        }
     } else {
         uint32_t a = nondet_uint32_t();
         uint32_t b = nondet_uint32_t();
-        if (aws_add_u32_checked(a, b, &r) == AWS_OP_SUCCESS && __aws_add_u32_checked(a, b, &__r) == AWS_OP_SUCCESS)
-            assert(r == __r);
+        uint32_t r = nondet_uint32_t();
+        if (!aws_add_u32_checked(a, b, &r)) {
+            assert(r == a + b);
+        } else {
+            assert((b > 0) && (a > (UINT32_MAX - b)));
+        }
     }
 }
--- .cbmc-batch/jobs/aws_mul_size_saturating/aws_mul_size_saturating_harness.c
+++ .cbmc-batch/jobs/aws_mul_size_saturating/aws_mul_size_saturating_harness.c
@@ -2,46 +2,42 @@
 #include <proof_helpers/make_common_data_structures.h>
 
 /**
- * Multiplies a * b. If the result overflows, returns 2^64 - 1.
- */
-AWS_STATIC_IMPL uint64_t __aws_mul_u64_saturating(uint64_t a, uint64_t b) {
-    uint64_t x = a * b;
-    if (a != 0 && (a > UINT32_MAX || b > UINT32_MAX) && x / a != b) {
-        return UINT64_MAX;
-    }
-    return x;
-}
-
-/**
- * Multiplies a * b. If the result overflows, returns 2^32 - 1.
- */
-AWS_STATIC_IMPL uint32_t __aws_mul_u32_saturating(uint32_t a, uint32_t b) {
-    uint32_t x = a * b;
-    if (a != 0 && (a > UINT16_MAX || b > UINT16_MAX) && x / a != b) {
-        return UINT32_MAX;
-    }
-    return x;
-}
-
-/**
- * Runtime: 0m3.393s
+ * Coverage: 1.00 (24 lines out of 24 statically-reachable lines in 3 functions reached)
+ * Runtime: 0m2.962s
+ *
+ * Assumptions:
+ *     - given 2 non-deterministics unsigned integers
+ *
+ * Assertions:
+ *     - if a * b overflows, aws_mul_u32_saturating and aws_mul_u64_saturating
+ *       functions must always return the corresponding saturated value
  */
 void aws_mul_size_saturating_harness() {
-    if (nondet_int()) {
+    if (nondet_bool()) {
         /*
          * In this particular case, full range of nondet inputs leads
-         * to excessively long runtimes, so use UINT64_MAX instead.
+         * to excessively long runtimes, so use 0 or UINT64_MAX instead.
          */
-        uint64_t a = UINT64_MAX;
+        uint64_t a = (nondet_int()) ? 0 : UINT64_MAX;
         uint64_t b = nondet_uint64_t();
-        assert(aws_mul_u64_saturating(a, b) == __aws_mul_u64_saturating(a, b));
+        uint64_t r = aws_mul_u64_saturating(a, b);
+        if (a > 0 && b > 0 && a > (UINT64_MAX / b)) {
+            assert(r == UINT64_MAX);
+        } else {
+            assert(r == a * b);
+        }
     } else {
         /*
          * In this particular case, full range of nondet inputs leads
-         * to excessively long runtimes, so use UINT32_MAX instead.
+         * to excessively long runtimes, so use 0 or UINT32_MAX instead.
          */
-        uint32_t a = UINT32_MAX;
+        uint32_t a = (nondet_bool()) ? 0 : UINT32_MAX;
         uint32_t b = nondet_uint32_t();
-        assert(aws_mul_u32_saturating(a, b) == __aws_mul_u32_saturating(a, b));
+        uint32_t r = aws_mul_u32_saturating(a, b);
+        if (a > 0 && b > 0 && a > (UINT32_MAX / b)) {
+            assert(r == UINT32_MAX);
+        } else {
+            assert(r == a * b);
+        }
     }
 }
