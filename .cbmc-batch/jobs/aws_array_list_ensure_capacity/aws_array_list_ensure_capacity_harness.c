28038e6f364ef3b60d8fb9afad0dc3b86b9aa2f5
Felipe R. Monteiro <felisous@amazon.com>
2019-04-17 13:30:23 -0400

    Refactor aws_array_list harnesses with new proof style
    Refactored functions:
     - aws_array_list_ensure_capacity
     - aws_array_list_front
     - aws_array_list_get_at
     - aws_array_list_get_at_ptr
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/jobs/aws_array_list_ensure_capacity/aws_array_list_ensure_capacity_harness.c
+++ .cbmc-batch/jobs/aws_array_list_ensure_capacity/aws_array_list_ensure_capacity_harness.c
@@ -16,43 +16,33 @@
 #include <aws/common/array_list.h>
 #include <proof_helpers/make_common_data_structures.h>
 
-/* These values allow us to reach a higher coverage rate */
-#define MAX_ITEM_SIZE 2
-#define MAX_INITIAL_ITEM_ALLOCATION (UINT64_MAX / MAX_ITEM_SIZE) + 1
-
 /**
- * Runtime: 0m9.181s
+ * Runtime: 9s
  */
 void aws_array_list_ensure_capacity_harness() {
-    struct aws_array_list *list;
-    /*
-     * Assumptions:
-     *     - a valid non-deterministic aws_array_list bounded by initial_item_allocation and item_size;
-     *     - non-deterministic list->initial_item_allocation <= MAX_INITIAL_ITEM_ALLOCATION;
-     *     - non-deterministic list->item_size <= MAX_ITEM_SIZE;
-     *     - non-deterministic list->length <= initial_item_allocation;
-     */
-    ASSUME_BOUNDED_ARRAY_LIST(list, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE);
+    /* data structure */
+    struct aws_array_list list;
 
-    struct aws_allocator *alloc = list->alloc;
-    size_t current_size = list->current_size;
-    size_t length = list->length;
-    size_t item_size = list->item_size;
-    void *data = list->data;
+    /* assumptions */
+    __CPROVER_assume(aws_array_list_is_bounded(&list, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE));
+    ensure_array_list_has_allocated_data_member(&list);
+    __CPROVER_assume(aws_array_list_is_valid(&list));
 
-    size_t index = nondet_size_t();
+    /* save current state of the data structure */
+    struct aws_array_list old = list;
+    struct store_byte_from_buffer old_byte;
+    save_byte_from_array((uint8_t *)list.data, list.current_size, &old_byte);
 
-    if (!aws_array_list_ensure_capacity(list, index)) {
-        size_t necessary_size = (index + 1) * list->item_size;
-        if (current_size < necessary_size) {
-            size_t next_allocation_size = current_size << 1;
-            size_t new_size = next_allocation_size > necessary_size ? next_allocation_size : necessary_size;
-            assert(list->current_size == new_size);
-        }
+    /* perform operation under verification */
+    size_t index;
+    if (!aws_array_list_ensure_capacity(&list, index)) {
+        /* assertions */
+        assert(aws_array_list_is_valid(&list));
+        assert(list.item_size == old.item_size);
+        assert(list.alloc == old.alloc);
+        assert(list.length == old.length);
+        assert(list.current_size >= old.current_size);
     } else {
-        assert(list->current_size == current_size);
+        assert_array_list_equivalence(&list, &old, &old_byte);
     }
-    assert(list->item_size == item_size);
-    assert(list->alloc == alloc);
-    assert(list->length == length);
 }
