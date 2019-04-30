55e601db4a8b5ac02c6268961da258c7eb7122a4
Felipe R. Monteiro <felisous@amazon.com>
2019-04-30 19:42:05 -0400

    Fixes implementation of the aws_byte_buf_init_copy_from_cursor function and its invariant
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ .cbmc-batch/jobs/aws_byte_buf_init_copy_from_cursor/aws_byte_buf_init_copy_from_cursor_harness.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/common/byte_buf.h>
+#include <proof_helpers/make_common_data_structures.h>
+#include <proof_helpers/proof_allocators.h>
+
+void aws_byte_buf_init_copy_from_cursor_harness() {
+    /* data structure */
+    struct aws_byte_buf *buf;
+
+    /* parameters */
+    struct aws_allocator *allocator;
+    struct aws_byte_cursor cursor;
+
+    /* assumptions */
+    __CPROVER_assume(is_bounded_byte_cursor(&cursor, MAX_BUFFER_SIZE));
+    ensure_byte_cursor_has_allocated_buffer_member(&cursor);
+    __CPROVER_assume(aws_byte_cursor_is_valid(&cursor));
+    ASSUME_VALID_MEMORY(buf);
+    ASSUME_CAN_FAIL_ALLOCATOR(allocator);
+
+    if (!aws_byte_buf_init_copy_from_cursor(buf, allocator, cursor)) {
+        /* assertions */
+        assert(aws_byte_buf_is_valid(buf));
+        assert(buf->len == cursor.len);
+        assert(buf->capacity == cursor.len);
+        assert(buf->allocator == allocator);
+        assert_bytes_match(buf->buffer, cursor.ptr, buf->len);
+    }
+    /* assertions */
+    assert(aws_byte_cursor_is_valid(&cursor));
+}
