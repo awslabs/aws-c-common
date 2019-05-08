3b57352591e1b6c4bfd4b8585294bfa2fde164e9
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-05-08 16:36:51 -0400

    PR Comments

--- .cbmc-batch/jobs/aws_hash_table_get_entry_count/aws_hash_table_get_entry_count_harness.c
+++ .cbmc-batch/jobs/aws_hash_table_get_entry_count/aws_hash_table_get_entry_count_harness.c
@@ -22,18 +22,19 @@
 
 void aws_hash_table_get_entry_count_harness() {
     struct aws_hash_table table;
-    // There are no loops in the code under test, so use the biggest possible value
+    /* There are no loops in the code under test, so use the biggest possible value */
     ensure_allocated_hash_table(&table, SIZE_MAX);
     __CPROVER_assume(aws_hash_table_is_valid(&table));
+    struct hash_table_state *state = table.p_impl;
 
     struct store_byte_from_buffer stored_byte;
     save_byte_from_hash_table(&table, &stored_byte);
-
-    aws_hash_table_get_entry_count(&table);
-
-    // Ensure that the table remains valid
+    size_t old_entry_count = state->entry_count;
+    size_t rval = aws_hash_table_get_entry_count(&table);
+    assert(rval == old_entry_count);
+    /* Ensure that the table remains valid */
     assert(aws_hash_table_is_valid(&table));
 
-    // Ensure that table is unchanged
+    /* Ensure that table is unchanged */
     check_hash_table_unchanged(&table, &stored_byte);
 }
