178486797c7ac2f6ca709e3f147b033457e3991e
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-05-09 13:29:30 -0400

    update the harness

--- .cbmc-batch/jobs/aws_byte_buf_reset/aws_byte_buf_reset_harness.c
+++ .cbmc-batch/jobs/aws_byte_buf_reset/aws_byte_buf_reset_harness.c
@@ -13,15 +13,17 @@
  * permissions and limitations under the License.
  */
 
- #include <aws/common/byte_buf.h>
+#include <aws/common/byte_buf.h>
 #include <proof_helpers/make_common_data_structures.h>
+#include <proof_helpers/utils.h>
 
- void aws_byte_buf_reset_harness() {
+void aws_byte_buf_reset_harness() {
     struct aws_byte_buf buf;
 
-    __CPROVER_assume(aws_byte_buf_is_bounded(&buf, MAX_BUFFER_SIZE));
-     ensure_byte_buf_has_allocated_buffer_member(&buf);
+    __CPROVER_assume(is_bounded_byte_buf(&buf, MAX_BUFFER_SIZE));
+    ensure_byte_buf_has_allocated_buffer_member(&buf);
     __CPROVER_assume(aws_byte_buf_is_valid(&buf));
+    assert(buf.capacity <= MAX_BUFFER_SIZE);
 
     struct aws_byte_buf old = buf;
     bool zero_contents;
@@ -30,4 +32,7 @@
     assert(buf.allocator == old.allocator);
     assert(buf.buffer == old.buffer);
     assert(buf.capacity == old.capacity);
+    if (zero_contents) {
+        assert_all_bytes_are(buf.buffer, 0, buf.capacity);
+    }
 }
