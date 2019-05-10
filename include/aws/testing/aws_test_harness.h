4e762a302982184efd3fe91bbb09ef94a5d00a25
Justin Boswell <boswej@amazon.com>
2019-05-09 20:20:05 -0700

    Windows compile fixes

--- include/aws/testing/aws_test_harness.h
+++ include/aws/testing/aws_test_harness.h
@@ -345,7 +345,6 @@ struct aws_test_harness {
 
 #if defined(_WIN32)
 #include <windows.h>
-#include <dbghelp.h>
 static LONG WINAPI s_test_print_stack_trace(struct _EXCEPTION_POINTERS *exception_pointers) {
 #if !defined(AWS_HEADER_CHECKER)
     aws_backtrace_print(stderr, exception_pointers);
