0abd184f0af34ba0ecd24aedecade704fae6464c
Justin Boswell <boswej@amazon.com>
2019-05-09 20:07:36 -0700

    What are you talking about AL2012

--- tests/system_info_tests.c
+++ tests/system_info_tests.c
@@ -73,6 +73,7 @@ static int s_test_stack_trace_decoding(struct aws_allocator *allocator, void *ct
         found_file_line |= strstr(buffer, fileline) != NULL;
     }
 
+    ASSERT_INT_EQUALS(line, __LINE__);
     ASSERT_TRUE(found_file_line);
 #endif
 
