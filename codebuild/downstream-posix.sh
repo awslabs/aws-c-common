c09d7a44e5320fbbb325be3384161f5883d82e44
Justin Boswell <boswej@amazon.com>
2019-05-13 10:39:47 -0700

    dammit git

--- codebuild/downstream-posix.sh
+++ codebuild/downstream-posix.sh
@@ -15,7 +15,7 @@ build_dir=$home_dir/build/downstream
 # where deps will be installed
 install_prefix=$build_dir/install
 
-common_branch_name=$(git rev-parse --abbrev-ref HEAD)
+common_branch_name=$(git branch | grep \* | cut -d ' ' -f2)
 
 cmake_args=""
 
