f28fac488b406d1dbfdbe131732dea8e4f4b24c4
Henson <henso@amazon.com>
2019-05-15 11:28:30 -0700

    Fix syntax.

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,17 +1,17 @@
 
 mkdir %TEMP%\build
 cd  %TEMP%\build
-cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="RelWithDebugInfo" %CODEBUILD_SRC_DIR% || goto error
-msbuild.exe aws-c-common.vcxproj /p:Configuration=RelWithDebugInfo || goto error
-msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
-msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
+cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" %CODEBUILD_SRC_DIR% || goto error
+msbuild.exe aws-c-common.vcxproj /p:Configuration=RelWithDebInfo || goto error
+msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=RelWithDebInfo || goto error
+msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=RelWithDebInfo || goto error
 ctest -V || goto error
 
 del /S /F .\\**
-cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="RelWithDebugInfo" -DBUILD_SHARED_LIBS=ON %CODEBUILD_SRC_DIR% || goto error
-msbuild.exe aws-c-common.vcxproj /p:Configuration=RelWithDebugInfo || goto error
-msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
-msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
+cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DBUILD_SHARED_LIBS=ON %CODEBUILD_SRC_DIR% || goto error
+msbuild.exe aws-c-common.vcxproj /p:Configuration=RelWithDebInfo || goto error
+msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=RelWithDebInfo || goto error
+msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=RelWithDebInfo || goto error
 ctest -V || goto error
 
 goto :EOF
