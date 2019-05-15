0079e2d5bb58f1dff2627529cb368e7d4ec065f9
Henson <henso@amazon.com>
2019-05-15 11:23:24 -0700

    Address PR feedback, hopefully fix windows build failure?

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -5,11 +5,12 @@ set -e
 mkdir /tmp/build
 cd /tmp/build
 
-cmake -DPERFORM_HEADER_CHECK=ON -DENABLE_SANITIZERS=ON $@ $CODEBUILD_SRC_DIR
-make -j 12
+CMAKE_ARGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DPERFORM_HEADER_CHECK=ON -DENABLE_SANITIZERS=ON $@"
+cmake $CMAKE_ARGS  $CODEBUILD_SRC_DIR
+make -j
 ctest . --output-on-failure
 rm -rf ./*
-cmake -DPERFORM_HEADER_CHECK=ON -DENABLE_SANITIZER=ON -DBUILD_SHARED_LIBS=ON $@ $CODEBUILD_SRC_DIR
-make -j 12
+cmake  -DBUILD_SHARED_LIBS=ON $CMAKE_ARGS $CODEBUILD_SRC_DIR
+make -j
 ctest . --output-on-failure
 
--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,16 +1,17 @@
 
 mkdir %TEMP%\build
 cd  %TEMP%\build
-cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="Release" %CODEBUILD_SRC_DIR% || goto error
-msbuild.exe aws-c-common.vcxproj /p:Configuration=Release || goto error
-msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=Release || goto error
-msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=Release || goto error
+cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="RelWithDebugInfo" %CODEBUILD_SRC_DIR% || goto error
+msbuild.exe aws-c-common.vcxproj /p:Configuration=RelWithDebugInfo || goto error
+msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
+msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
 ctest -V || goto error
 
-cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="Release" -DBUILD_SHARED_LIBS=ON %CODEBUILD_SRC_DIR% || goto error
-msbuild.exe aws-c-common.vcxproj /p:Configuration=Release || goto error
-msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=Release || goto error
-msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=Release || goto error
+del /S /F .\\**
+cmake %* -DPERFORM_HEADER_CHECK=ON -DCMAKE_BUILD_TYPE="RelWithDebugInfo" -DBUILD_SHARED_LIBS=ON %CODEBUILD_SRC_DIR% || goto error
+msbuild.exe aws-c-common.vcxproj /p:Configuration=RelWithDebugInfo || goto error
+msbuild.exe tests/aws-c-common-assert-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
+msbuild.exe tests/aws-c-common-tests.vcxproj /p:Configuration=RelWithDebugInfo || goto error
 ctest -V || goto error
 
 goto :EOF
