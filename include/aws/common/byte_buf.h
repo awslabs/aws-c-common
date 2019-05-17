e3ee150a5951af746d78fcc06776fe62d9f54876
Felipe R. Monteiro <felisous@amazon.com>
2019-05-17 16:45:11 -0400

    Adds proof harness for aws_byte_buf_advance function
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- include/aws/common/byte_buf.h
+++ include/aws/common/byte_buf.h
@@ -780,13 +780,16 @@ AWS_STATIC_IMPL bool aws_byte_buf_advance(
     struct aws_byte_buf *AWS_RESTRICT buffer,
     struct aws_byte_buf *AWS_RESTRICT output,
     size_t len) {
+    AWS_PRECONDITION(aws_byte_buf_is_valid(buffer) && aws_byte_buf_is_valid(output));
     if (buffer->capacity - buffer->len >= len) {
         *output = aws_byte_buf_from_array(buffer->buffer + buffer->len, len);
         buffer->len += len;
         output->len = 0;
+        AWS_POSTCONDITION(aws_byte_buf_is_valid(buffer) && aws_byte_buf_is_valid(output));
         return true;
     } else {
         AWS_ZERO_STRUCT(*output);
+        AWS_POSTCONDITION(aws_byte_buf_is_valid(buffer) && aws_byte_buf_is_valid(output));
         return false;
     }
 }
