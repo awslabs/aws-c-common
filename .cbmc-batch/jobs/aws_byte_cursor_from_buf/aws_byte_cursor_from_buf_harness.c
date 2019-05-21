b8cb8230b43c21fc66aa55b8ce6a50b9c8f88c50
Felipe R. Monteiro <felisous@amazon.com>
2019-05-21 16:01:54 -0400

    Adds proof harnesses for aws_byte_cursor_from_* functions
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ .cbmc-batch/jobs/aws_byte_cursor_from_buf/aws_byte_cursor_from_buf_harness.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/common/byte_buf.h>
+#include <proof_helpers/make_common_data_structures.h>
+
+void aws_byte_cursor_from_buf_harness() {
+    /* parameter */
+    struct aws_byte_buf buf;
+
+    /* assumptions */
+    __CPROVER_assume(aws_byte_buf_is_bounded(&buf, MAX_BUFFER_SIZE));
+    ensure_byte_buf_has_allocated_buffer_member(&buf);
+    __CPROVER_assume(aws_byte_buf_is_valid(&buf));
+
+    /* save current state of the parameters */
+    struct aws_byte_buf old = buf;
+    struct store_byte_from_buffer old_byte_from_buf;
+    save_byte_from_array(buf.buffer, buf.len, &old_byte_from_buf);
+
+    /* operation under verification */
+    struct aws_byte_cursor cur = aws_byte_cursor_from_buf(&buf);
+
+    /* assertions */
+    assert(aws_byte_buf_is_valid(&buf));
+    assert(aws_byte_cursor_is_valid(&cur));
+    assert_byte_buf_equivalence(&buf, &old, &old_byte_from_buf);
+    assert(cur.len == buf.len);
+    if (cur.ptr) {
+        assert_bytes_match(cur.ptr, buf.buffer, buf.len);
+    }
+}
