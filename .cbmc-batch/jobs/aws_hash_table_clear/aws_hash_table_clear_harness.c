bcbc930a21cf596da572702f5c8cb6922e1a4919
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2019-05-23 13:08:47 -0400

    CBMC proofs for aws_hash_table_clear() and aws_hash_table_clean_up() (#354)
    * aws_hash_table_clear CBMC proof and code cleanup.
    
    * hash_table_cleanup CBMC proof
    
    * PR Comments
    
    * removed unnecessary commented line

---
+++ .cbmc-batch/jobs/aws_hash_table_clear/aws_hash_table_clear_harness.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <aws/common/hash_table.h>
+#include <aws/common/private/hash_table_impl.h>
+#include <proof_helpers/make_common_data_structures.h>
+#include <proof_helpers/proof_allocators.h>
+#include <proof_helpers/utils.h>
+
+void aws_hash_table_clear_harness() {
+    struct aws_hash_table map;
+    ensure_allocated_hash_table(&map, MAX_TABLE_SIZE);
+    __CPROVER_assume(aws_hash_table_is_valid(&map));
+    ensure_hash_table_has_valid_destroy_functions(&map);
+
+    size_t empty_slot_idx;
+    __CPROVER_assume(aws_hash_table_has_an_empty_slot(&map, &empty_slot_idx));
+    aws_hash_table_clear(&map);
+    assert(aws_hash_table_is_valid(&map));
+    struct hash_table_state *impl = map.p_impl;
+    assert_all_zeroes(&impl->slots[0], impl->size * sizeof(impl->slots[0]));
+}
