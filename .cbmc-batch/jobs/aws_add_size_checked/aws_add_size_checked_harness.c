39e6ebc7fffb9e3163a1b599f791fe6920bfa2fb
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-05-23 14:49:10 -0400

    clang-format

--- .cbmc-batch/jobs/aws_add_size_checked/aws_add_size_checked_harness.c
+++ .cbmc-batch/jobs/aws_add_size_checked/aws_add_size_checked_harness.c
@@ -17,7 +17,7 @@ void aws_add_size_checked_harness() {
         uint64_t a = nondet_uint64_t();
         uint64_t b = nondet_uint64_t();
         uint64_t r = nondet_uint64_t();
-	int rval = aws_add_u64_checked(a, b, &r);
+        int rval = aws_add_u64_checked(a, b, &r);
         if (!rval) {
             assert(r == a + b);
         } else {
