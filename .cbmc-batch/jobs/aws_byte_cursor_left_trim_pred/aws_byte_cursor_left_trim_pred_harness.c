03ffb8603589262aecfdb6f3cac8d1e25ed3d36b
Felipe R. Monteiro <felisous@amazon.com>
2019-06-03 21:11:51 -0400

    Adds a proof harness for aws_byte_cursor_left_trim_pred function
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/jobs/aws_byte_cursor_left_trim_pred/aws_byte_cursor_left_trim_pred_harness.c
+++ .cbmc-batch/jobs/aws_byte_cursor_left_trim_pred/aws_byte_cursor_left_trim_pred_harness.c
@@ -17,7 +17,7 @@
 #include <proof_helpers/make_common_data_structures.h>
 #include <proof_helpers/proof_allocators.h>
 
-void aws_byte_cursor_trim_pred_harness() {
+void aws_byte_cursor_left_trim_pred_harness() {
     /* parameters */
     struct aws_byte_cursor cur;
 
@@ -31,7 +31,7 @@ void aws_byte_cursor_trim_pred_harness() {
     save_byte_from_array(cur.ptr, cur.len, &old_byte_from_cur);
 
     /* operation under verification */
-    struct aws_byte_cursor rv = aws_byte_cursor_trim_pred(nondet_bool() ? &cur : NULL, nondet_bool() ? predicate_fn : NULL);
+    struct aws_byte_cursor rv = aws_byte_cursor_left_trim_pred(nondet_bool() ? &cur : NULL, nondet_bool() ? predicate_fn : NULL);
 
     /* assertions */
     assert(aws_byte_cursor_is_valid(&cur));
