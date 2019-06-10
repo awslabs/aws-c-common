3919c7b5fd6d1f0b88e89a49c9e8c0ae590c4ba2
Felipe R. Monteiro <felisous@amazon.com>
2019-06-10 10:49:58 -0400

    Adds a proof harness for aws_ring_buffer_release function
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ .cbmc-batch/jobs/aws_ring_buffer_release/aws_ring_buffer_release_harness.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/common/byte_buf.h>
+#include <aws/common/ring_buffer.h>
+#include <proof_helpers/make_common_data_structures.h>
+#include <proof_helpers/proof_allocators.h>
+
+void aws_ring_buffer_release_harness() {
+    /* parameters */
+    struct aws_ring_buffer ring_buf;
+    size_t ring_buf_size;
+    struct aws_byte_buf buf;
+
+    /* assumptions */
+    ensure_ring_buffer_has_allocated_members(&ring_buf, ring_buf_size);
+    __CPROVER_assume(aws_ring_buffer_is_valid(&ring_buf));
+    ensure_byte_buf_has_allocated_buffer_member(&buf);
+    __CPROVER_assume(aws_byte_buf_is_valid(&buf));
+    __CPROVER_assume(
+        aws_ring_buffer_acquire_up_to(&ring_buf, nondet_size_t(), nondet_size_t(), &buf) == AWS_OP_SUCCESS);
+
+    aws_ring_buffer_release(nondet_bool() ? &ring_buf : NULL, nondet_bool() ? &buf : NULL);
+
+    /* assertions */
+    assert(buf.allocator == NULL);
+    assert(buf.buffer == NULL);
+    assert(buf.len == 0);
+    assert(buf.capacity == 0);
+}
