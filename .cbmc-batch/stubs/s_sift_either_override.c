2de95b4b348c92cc2e9981766dc16450c25ad407
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-06-14 16:50:04 -0400

    Add an explanation in the s_sift_either stub

--- .cbmc-batch/stubs/s_sift_either_override.c
+++ .cbmc-batch/stubs/s_sift_either_override.c
@@ -18,14 +18,22 @@
  *
  * This function overrides the original implementation of the
  * s_sift_either function from priority_queue.h with a no_op.
+ *
+ * It is necessary to stub out s_sift_either because in order for it
+ * to work, we have to have initialized all backpointers of the
+ * priority queue correctly (which needs a loop).
+ *
+ * It is safe to stub out s_sift_either as long as we don't make any
+ * assertions on the positions of elements in the priority queue and
+ * their relative values.
+ *
  */
 
 #include <aws/common/priority_queue.h>
 
 bool __CPROVER_file_local_priority_queue_c_s_sift_either(struct aws_priority_queue *queue, size_t index) {
-    AWS_PRECONDITION(aws_priority_queue_is_valid(queue));
-    AWS_PRECONDITION(index < queue->container.length);
+    assert(aws_priority_queue_is_valid(queue));
+    assert(index < queue->container.length);
     bool did_move;
-    AWS_POSTCONDITION(aws_priority_queue_is_valid(queue));
     return did_move;
 }
