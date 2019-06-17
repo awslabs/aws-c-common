40f1402c8e88ae3c063b073f204b7bf236f01915
lucasmt <lucasmt@amazon.com>
2019-06-17 11:17:34 -0400

    Added assumptions to proof harness of aws_byte_cursor_read_be* functions

--- .cbmc-batch/include/proof_helpers/aws_byte_cursor_read_common.h
+++ .cbmc-batch/include/proof_helpers/aws_byte_cursor_read_common.h
@@ -26,17 +26,19 @@ void aws_byte_cursor_read_common_harness() {
     /* assumptions */
     ensure_byte_cursor_has_allocated_buffer_member(&cur);
     __CPROVER_assume(aws_byte_cursor_is_valid(&cur));
+    __CPROVER_assume(AWS_MEM_IS_READABLE(cur.ptr, BYTE_WIDTH));
+    __CPROVER_assume(AWS_OBJECT_PTR_IS_WRITABLE(dest));
 
     /* save current state of the data structure */
     struct aws_byte_cursor old_cur = cur;
     struct store_byte_from_buffer old_byte_from_cur;
     save_byte_from_array(cur.ptr, cur.len, &old_byte_from_cur);
+    DEST_TYPE dest_copy;
+    memcpy(&dest_copy, old_cur.ptr, BYTE_WIDTH);
+    dest_copy = AWS_NTOH(dest_copy);
 
     /* operation under verification */
-    if (BYTE_CURSOR_READ((nondet_bool() ? &cur : NULL), dest)) {
-        DEST_TYPE dest_copy;
-        memcpy(&dest_copy, old_cur.ptr, BYTE_WIDTH);
-        dest_copy = AWS_NTOH(dest_copy);
+    if (BYTE_CURSOR_READ(&cur, dest)) {
         assert_bytes_match((uint8_t *)&dest_copy, (uint8_t *)dest, BYTE_WIDTH);
         /* the following assertions are included, because aws_byte_cursor_read internally uses
          * aws_byte_cursor_advance_nospec and it copies the bytes from the advanced cursor to *dest
