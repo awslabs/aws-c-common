5f115ff6b1e5c837dd9aa67cae60fa476053a833
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-06-18 15:12:25 -0400

    Add comments in the s_remove_node stub

--- .cbmc-batch/stubs/s_remove_node_override.c
+++ .cbmc-batch/stubs/s_remove_node_override.c
@@ -34,12 +34,22 @@ int __CPROVER_file_local_priority_queue_c_s_remove_node(
     size_t item_index) {
     assert(aws_priority_queue_is_valid(queue));
     assert(item && AWS_MEM_IS_WRITABLE(item, queue->container.item_size));
+
+    /* If aws_array_list_get_at succeeds, it means that the item_index
+     * is in range of the container, and thus the queue has at least
+     * item_index + 1 elements */
     if (aws_array_list_get_at(&queue->container, item, item_index)) {
         /* shouldn't happen, but if it does we've already raised an error... */
         assert(aws_priority_queue_is_valid(queue));
         return AWS_OP_ERR;
     }
 
+    /* This can never underflow, as aws_array_list_get_at has
+     * succeeded, which means that the container has at least one
+     * item. Also if the backpointers array_list is initialized, it is
+     * constrained to have the same length as the container array_list
+     * (as queue is a valid priority queue) and thus is guaranteed to
+     * not underflow. */
     queue->container.length -= 1;
     if (queue->backpointers.data) {
         queue->backpointers.length -= 1;
