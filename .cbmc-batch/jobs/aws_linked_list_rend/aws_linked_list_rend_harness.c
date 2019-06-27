23a3a28aec125fd242fb873588c1c0364b7578a1
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-06-27 13:49:03 -0400

    Sort lines and explain an assumption

--- .cbmc-batch/jobs/aws_linked_list_rend/aws_linked_list_rend_harness.c
+++ .cbmc-batch/jobs/aws_linked_list_rend/aws_linked_list_rend_harness.c
@@ -21,7 +21,11 @@ void aws_linked_list_rend_harness() {
     struct aws_linked_list list;
 
     ensure_linked_list_is_allocated(&list, MAX_LINKED_LIST_ITEM_ALLOCATION);
+
+    /* Assume the preconditions */
     __CPROVER_assume(aws_linked_list_is_valid(&list));
+
+    /* Note: list can never be a NULL pointer as is_valid checks for that */
 
     /* perform operation under verification */
     struct aws_linked_list_node *rval = aws_linked_list_rend(&list);
