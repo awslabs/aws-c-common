87ee7fb8d5494d998ec738921814816e520baf47
Felipe R. Monteiro <felisous@amazon.com>
2019-06-27 19:25:40 -0400

    Adds a proof harness for aws_string_eq_byte_cursor_ignore_case
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ .cbmc-batch/jobs/aws_string_eq_byte_cursor_ignore_case/aws_string_eq_byte_cursor_ignore_case_harness.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <aws/common/string.h>
+#include <proof_helpers/make_common_data_structures.h>
+#include <proof_helpers/proof_allocators.h>
+
+void aws_string_eq_byte_cursor_ignore_case_harness() {
+    struct aws_string *str = make_arbitrary_aws_string_nondet_len_with_max(MAX_STRING_LEN);
+    struct aws_byte_cursor cursor;
+
+    ensure_byte_cursor_has_allocated_buffer_member(&cursor);
+    __CPROVER_assume(aws_byte_cursor_is_valid(&cursor));
+
+    if (aws_string_eq_byte_cursor_ignore_case(str, &cursor)) {
+        assert(str->len == cursor.len);
+    }
+
+    assert(aws_string_is_valid(str));
+    assert(aws_byte_cursor_is_valid(&cursor));
+}
