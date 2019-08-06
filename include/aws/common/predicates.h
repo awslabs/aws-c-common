8601f7229917a6f7f52b9d5e828e0e5146da0c83
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-08-06 16:00:37 -0400

    Make check in deep checks per Colden's comment

--- include/aws/common/predicates.h
+++ include/aws/common/predicates.h
@@ -18,16 +18,20 @@
 /**
  * Returns whether all bytes of the two byte arrays match.
  */
-#ifdef CBMC
+#if (AWS_DEEP_CHECKS == 1)
+#    ifdef CBMC
 /* clang-format off */
-#    define AWS_BYTES_EQ(arr1, arr2, len)                                                                              \
-        __CPROVER_forall {                                                                                             \
-            int i;                                                                                                     \
-            (i >= 0 && i < len) ==> ((const uint8_t *)&arr1)[i] == ((const uint8_t *)&arr2)[i]                         \
-        }
+#        define AWS_BYTES_EQ(arr1, arr2, len)                                                                              \
+            __CPROVER_forall {                                                                                             \
+                int i;                                                                                                     \
+                (i >= 0 && i < len) ==> ((const uint8_t *)&arr1)[i] == ((const uint8_t *)&arr2)[i]                         \
+            }
 /* clang-format on */
+#    else
+#        define AWS_BYTES_EQ(arr1, arr2, len) (memcmp(arr1, arr2, len) == 0)
+#    endif /* CBMC */
 #else
-#    define AWS_BYTES_EQ(arr1, arr2, len) (memcmp(arr1, arr2, len) == 0)
-#endif
+#    define AWS_BYTES_EQ(arr1, arr2, len) (1)
+#endif /* (AWS_DEEP_CHECKS == 1) */
 
-#endif /* AWS_PREDICATES_MACROS_H */
+#endif /* AWS_COMMON_PREDICATES_H */
