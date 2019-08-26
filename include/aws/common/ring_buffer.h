63e52604e593d5d7378cb820cd31c7ce7ea71f4b
Nathan Chong <ncchong@amazon.com>
2019-08-26 13:05:06 -0400

    Fix clang-format error

--- include/aws/common/ring_buffer.h
+++ include/aws/common/ring_buffer.h
@@ -26,16 +26,18 @@
         assert(aws_ring_buffer_check_atomic_ptr(ring_buf, src_ptr));                                                   \
         aws_atomic_store_ptr_explicit(atomic_ptr, src_ptr, memory_order);
 #else
-#    define AWS_ATOMIC_LOAD_PTR(ring_buf, dest_ptr, atomic_ptr, memory_order) dest_ptr = aws_atomic_load_ptr_explicit(atomic_ptr, memory_order);
-#    define AWS_ATOMIC_STORE_PTR(ring_buf, atomic_ptr, src_ptr, memory_order) aws_atomic_store_ptr_explicit(atomic_ptr, src_ptr, memory_order);
+#    define AWS_ATOMIC_LOAD_PTR(ring_buf, dest_ptr, atomic_ptr, memory_order)                                          \
+        dest_ptr = aws_atomic_load_ptr_explicit(atomic_ptr, memory_order);
+#    define AWS_ATOMIC_STORE_PTR(ring_buf, atomic_ptr, src_ptr, memory_order)                                          \
+        aws_atomic_store_ptr_explicit(atomic_ptr, src_ptr, memory_order);
 #endif
-#define AWS_ATOMIC_LOAD_TAIL_PTR(ring_buf, dest_ptr, atomic_ptr) \
+#define AWS_ATOMIC_LOAD_TAIL_PTR(ring_buf, dest_ptr, atomic_ptr)                                                       \
     AWS_ATOMIC_LOAD_PTR(ring_buf, dest_ptr, atomic_ptr, aws_memory_order_acquire);
-#define AWS_ATOMIC_STORE_TAIL_PTR(ring_buf, atomic_ptr, src_ptr) \
+#define AWS_ATOMIC_STORE_TAIL_PTR(ring_buf, atomic_ptr, src_ptr)                                                       \
     AWS_ATOMIC_STORE_PTR(ring_buf, atomic_ptr, src_ptr, aws_memory_order_release);
-#define AWS_ATOMIC_LOAD_HEAD_PTR(ring_buf, dest_ptr, atomic_ptr) \
+#define AWS_ATOMIC_LOAD_HEAD_PTR(ring_buf, dest_ptr, atomic_ptr)                                                       \
     AWS_ATOMIC_LOAD_PTR(ring_buf, dest_ptr, atomic_ptr, aws_memory_order_relaxed);
-#define AWS_ATOMIC_STORE_HEAD_PTR(ring_buf, atomic_ptr, src_ptr) \
+#define AWS_ATOMIC_STORE_HEAD_PTR(ring_buf, atomic_ptr, src_ptr)                                                       \
     AWS_ATOMIC_STORE_PTR(ring_buf, atomic_ptr, src_ptr, aws_memory_order_relaxed);
 
 /**
