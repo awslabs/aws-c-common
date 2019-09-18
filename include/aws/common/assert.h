952890ff1cd533f71aef2772208a46b44a65c4b0
Jonathan M. Henson <henso@amazon.com>
2019-09-18 16:50:19 -0700

    Fix stupid bad boolean logic in macro.

--- include/aws/common/assert.h
+++ include/aws/common/assert.h
@@ -45,8 +45,12 @@ AWS_EXTERN_C_END
             bool _result = (cond);                                                                                     \
             __builtin_assume(_result);                                                                                 \
         } while (false)
-#elif defined(__GNUC__) && __GNUC__ >= 4 && __GNUC_MINOR__ >= 5
-#    define AWS_ASSUME(cond) ((cond) ? (void)0 : __builtin_unreachable())
+#elif defined(__GNUC__)
+#    if __GNUC__ == 4 && __GNUC_MINOR__ <= 4
+#        define AWS_ASSUME(cond)
+#    else
+#        define AWS_ASSUME(cond) ((cond) ? (void)0 : __builtin_unreachable())
+#    endif
 #else
 #    define AWS_ASSUME(cond)
 #endif
