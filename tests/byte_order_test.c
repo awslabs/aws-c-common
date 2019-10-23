97081187e3c8744fc3baa86c71d1ece921e4bd83
Colden Cullen <colden@coldencullen.com>
2019-10-23 13:01:04 -0700

    Remove AWS_ALIGN, as it doesn't work reliably, add AWS_ALIGNED_TYPEDEF (#530)

--- tests/byte_order_test.c
+++ tests/byte_order_test.c
@@ -17,6 +17,10 @@
 
 #include <aws/testing/aws_test_harness.h>
 
+#ifdef _MSC_VER
+#    pragma warning(disable : 4324) /* structure was padded due to alignment specifier */
+#endif
+
 static int s_byte_swap_test_fn(struct aws_allocator *allocator, void *ctx) {
     (void)allocator;
     (void)ctx;
@@ -61,3 +65,24 @@ static int s_byte_swap_test_fn(struct aws_allocator *allocator, void *ctx) {
     return 0;
 }
 AWS_TEST_CASE(byte_swap_test, s_byte_swap_test_fn);
+
+AWS_ALIGNED_TYPEDEF(uint8_t, aligned_storage[64], 32);
+
+struct padding_disaster {
+    aligned_storage a;
+    uint8_t dumb;
+    aligned_storage b;
+};
+
+static int s_alignment_test_fn(struct aws_allocator *allocator, void *ctx) {
+    (void)allocator;
+    (void)ctx;
+
+    struct padding_disaster padded;
+
+    ASSERT_UINT_EQUALS(0, ((intptr_t)&padded.a) % 32);
+    ASSERT_UINT_EQUALS(0, ((intptr_t)&padded.b) % 32);
+
+    return 0;
+}
+AWS_TEST_CASE(alignment_test, s_alignment_test_fn)
