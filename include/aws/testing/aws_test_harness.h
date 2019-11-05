7635ba375a4a6627250cef99f76c92c462b236e3
Justin Boswell <boswej@amazon.com>
2019-11-04 16:09:20 -0800

    Fixed crash in logger

--- include/aws/testing/aws_test_harness.h
+++ include/aws/testing/aws_test_harness.h
@@ -359,6 +359,7 @@ static inline int s_aws_run_test_case(struct aws_test_harness *harness) {
     struct aws_logger_standard_options options;
     options.file = AWS_TESTING_REPORT_FD;
     options.level = AWS_LL_TRACE;
+    options.filename = NULL;
     aws_logger_init_standard(&err_logger, aws_default_allocator(), &options);
     aws_logger_set(&err_logger);
 
