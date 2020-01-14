3841d251fe0bcca1d2eafc94db06b7febae44b03
Bret Ambrose <bretambrose@gmail.com>
2020-01-14 10:10:23 -0800

    Split alignment tests into 16 and 32 bytes; 32 enabled only if avx2 support is active

--- tests/byte_order_test.c
+++ tests/byte_order_test.c
@@ -66,23 +66,44 @@ static int s_byte_swap_test_fn(struct aws_allocator *allocator, void *ctx) {
 }
 AWS_TEST_CASE(byte_swap_test, s_byte_swap_test_fn);
 
-AWS_ALIGNED_TYPEDEF(uint8_t, aligned_storage[64], 32);
+AWS_ALIGNED_TYPEDEF(uint8_t, aligned32_storage[64], 32);
 
-struct padding_disaster {
-    aligned_storage a;
+struct padding32_disaster {
+    aligned32_storage a;
     uint8_t dumb;
-    aligned_storage b;
+    aligned32_storage b;
 };
 
-static int s_alignment_test_fn(struct aws_allocator *allocator, void *ctx) {
+static int s_alignment32_test_fn(struct aws_allocator *allocator, void *ctx) {
     (void)allocator;
     (void)ctx;
 
-    struct padding_disaster padded;
+    struct padding32_disaster padded;
 
     ASSERT_UINT_EQUALS(0, ((intptr_t)&padded.a) % 32);
     ASSERT_UINT_EQUALS(0, ((intptr_t)&padded.b) % 32);
 
     return 0;
 }
-AWS_TEST_CASE(alignment_test, s_alignment_test_fn)
+AWS_TEST_CASE(alignment32_test, s_alignment32_test_fn)
+
+AWS_ALIGNED_TYPEDEF(uint8_t, aligned16_storage[64], 16);
+
+struct padding16_disaster {
+    aligned16_storage a;
+    uint8_t dumb;
+    aligned16_storage b;
+};
+
+static int s_alignment16_test_fn(struct aws_allocator *allocator, void *ctx) {
+    (void)allocator;
+    (void)ctx;
+
+    struct padding16_disaster padded;
+
+    ASSERT_UINT_EQUALS(0, ((intptr_t)&padded.a) % 16);
+    ASSERT_UINT_EQUALS(0, ((intptr_t)&padded.b) % 16);
+
+    return 0;
+}
+AWS_TEST_CASE(alignment16_test, s_alignment16_test_fn)
