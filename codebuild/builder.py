88d4c699391fe714baa87ed03c9e605514893dc9
Justin Boswell <boswej@amazon.com>
2020-01-27 17:49:54 -0800

    Store refs to imported classes so the GC doesn't murder them

--- codebuild/builder.py
+++ codebuild/builder.py
@@ -562,7 +562,11 @@ def produce_config(build_spec, config_file, **additional_variables):
 # ACTIONS
 ########################################################################################################################
 class Builder(VirtualModule):
+    """ The interface available to scripts that define projects, builds, actions, or configuration """
+    # Must cache available actions or the GC will delete them
+    all_actions = set()
     def __init__(self):
+        Builder.all_actions = set(Builder.Action.__subclasses__())
         self._load_scripts()
 
     @staticmethod
@@ -575,11 +579,16 @@ class Builder(VirtualModule):
         
         scripts = glob.glob('.builder/**/*.py')
         for script in scripts:
-            print("Importing {}".format(os.path.abspath(script)))
+            print("Importing {}".format(os.path.abspath(script)), flush=True)
+            
             name = os.path.split(script)[1].split('.')[0]
             spec = importlib.util.spec_from_file_location(name, script)
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
+            actions = frozenset(Builder._find_actions())
+            new_actions = actions.difference(Builder.all_actions)
+            print("Imported {}".format(', '.join([a.__name__ for a in new_actions])))
+            Builder.all_actions.update(new_actions)
 
 
     @staticmethod
@@ -588,16 +597,24 @@ class Builder(VirtualModule):
 
     @staticmethod
     def find_action(name):
-        for action in Builder._find_actions():
-            if action.__name__.lower() == name.lower():
+        name = name.replace('-', '').lower()
+        all_actions = Builder._find_actions()
+        for action in all_actions:
+            if action.__name__.lower() == name:
                 return action
 
     @staticmethod
     def run_action(action, env):
         action_type = type(action)
         if action_type is str:
-            action_cls = Builder.find_action(action)
-            action = action_cls()
+            try:
+                action_cls = Builder.find_action(action)
+                action = action_cls()
+            except:
+                print("Unable to find action {} to run".format(action))
+                all_actions = [a.__name__ for a in Builder._find_actions()]
+                print("Available actions: \n\t{}".format('\n\t'.join(all_actions)))
+                sys.exit(2)
 
         print("Running: {}".format(action), flush=True)
         children = action.run(env)
