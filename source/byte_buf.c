755676b70bb186838ff32a0d950e95c2e3fe1942
Daniel Schwartz-Narbonne <dsn@amazon.com>
2020-03-02 15:51:02 -0500

    fix aws_byte_buf_write_from_whole_cursor CBMC proof, and minor issue in aws_byte_buf_write

--- source/byte_buf.c
+++ source/byte_buf.c
@@ -1263,6 +1263,11 @@ bool aws_byte_buf_write(struct aws_byte_buf *AWS_RESTRICT buf, const uint8_t *AW
     AWS_PRECONDITION(aws_byte_buf_is_valid(buf));
     AWS_PRECONDITION(AWS_MEM_IS_READABLE(src, len), "Input array [src] must be readable up to [len] bytes.");
 
+    if (len == 0) {
+	AWS_POSTCONDITION(aws_byte_buf_is_valid(buf));
+	return true;
+    }
+
     if (buf->len > (SIZE_MAX >> 1) || len > (SIZE_MAX >> 1) || buf->len + len > buf->capacity) {
         AWS_POSTCONDITION(aws_byte_buf_is_valid(buf));
         return false;
