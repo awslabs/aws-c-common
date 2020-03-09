a798f57a9e486b5d0139e117926b1f5899b17008
C. Mangla <cmangla@users.noreply.github.com>
2020-03-09 21:23:05 +0000

    Let user specify subset of jobs to be run (#607)
    Additionally, check for cbmc, cbmc-batch and aws. Plus minor fixes.

--- .cbmc-batch/check_result.py
+++ .cbmc-batch/check_result.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use
@@ -22,25 +23,25 @@ def check_result():
     parser.add_argument("yaml", help = "The yaml containing expected CBMC Batch result substring.")
     args = parser.parse_args()
     if not os.path.isfile(args.yaml):
-        print "Expected file " + args.yaml + ": Not found"
+        print("Expected file " + args.yaml + ": Not found")
         return
     with open(args.yaml, "r") as yaml_file:
         try:
             expected = yaml.load(yaml_file)["expected"]
         except yaml.YAMLError as e:
-            print e
+            print(e)
             return
         except KeyError as e:
-            print "Expected CBMC Batch result not found in " + args.yaml
+            print("Expected CBMC Batch result not found in " + args.yaml)
             return
     cbmc_output = os.path.join(args.batch_result_dir, "cbmc.txt")
     if not os.path.isfile(cbmc_output):
-        print "Expected file " + cbmc_output + ": Not found"
+        print("Expected file " + cbmc_output + ": Not found")
     with open(cbmc_output, "r") as cbmc:
         if expected in cbmc.read():
-            print "Expected output matches."
+            print("Expected output matches.")
         else:
-            print "Expected output does not match."
+            print("Expected output does not match.")
 
 if __name__ == "__main__":
     check_result()
