670819d3ea345cc3d531d0173ffeb5e975e2613a
Justin Boswell <boswej@amazon.com>
2020-04-02 10:48:25 -0700

    Removed unnecessary code

--- AWSCRTAndroidTestRunner/app/src/main/cpp/native-lib.cpp
+++ AWSCRTAndroidTestRunner/app/src/main/cpp/native-lib.cpp
@@ -6,42 +6,6 @@
 
 #include <android/log.h>
 
-static bool s_tests_finished = false;
-static size_t s_total_tests = 400;
-static size_t s_test_idx = 0;
-
-extern "C" JNIEXPORT void JNICALL
-Java_software_amazon_awssdk_crt_awscrtandroidtestrunner_MainActivity_startTests(
-        JNIEnv* env,
-        jobject /* this */) {
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_software_amazon_awssdk_crt_awscrtandroidtestrunner_MainActivity_currentTestName(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::stringstream test_name;
-    test_name << "TEST " << s_test_idx++;
-    if (s_test_idx >= s_total_tests) {
-        s_tests_finished = true;
-    }
-    return env->NewStringUTF(test_name.str().c_str());
-}
-
-extern "C" JNIEXPORT jboolean JNICALL
-Java_software_amazon_awssdk_crt_awscrtandroidtestrunner_MainActivity_testsFinished(
-        JNIEnv *env,
-        jobject /* this */) {
-    return s_tests_finished;
-}
-
-extern "C" JNIEXPORT jint JNICALL
-Java_software_amazon_awssdk_crt_awscrtandroidtestrunner_MainActivity_testsExitCode(
-        JNIEnv *env,
-        jobject /* this */) {
-    return 0;
-}
-
 typedef int(test_fn_t)(int, char**);
 
 extern "C" JNIEXPORT jint JNICALL
--- AWSCRTAndroidTestRunner/app/src/main/java/software/amazon/awssdk/crt/awscrtandroidtestrunner/MainActivity.kt
+++ AWSCRTAndroidTestRunner/app/src/main/java/software/amazon/awssdk/crt/awscrtandroidtestrunner/MainActivity.kt
@@ -3,40 +3,15 @@ package software.amazon.awssdk.crt.awscrtandroidtestrunner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.Timer
-import java.util.TimerTask
-import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-
-    val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         sample_text.text = "Starting Tests..."
-
-        startTests()
-        timer.schedule(object: TimerTask() {
-            override fun run() {
-                runOnUiThread{ updateStatus() }
-            }
-        }, 0, 100)
     }
-
-    fun updateStatus() {
-        sample_text.text = currentTestName()
-        if (testsFinished()) {
-            val exitCode = testsExitCode()
-            exitProcess(exitCode)
-        }
-    }
-
-    external fun startTests()
-    external fun currentTestName(): String
-    external fun testsFinished(): Boolean
-    external fun testsExitCode(): Int
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
