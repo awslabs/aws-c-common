c688fdf6b8899ee128389b6a6d98b60e99cd5a3c
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2020-04-27 16:44:26 -0400

    remove unnecessary uses of __CPROVER_allocated_memory (#627)

--- .cbmc-batch/jobs/aws_string_destroy_secure/aws_string_destroy_secure_harness.c
+++ .cbmc-batch/jobs/aws_string_destroy_secure/aws_string_destroy_secure_harness.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
  * this file except in compliance with the License. A copy of the License is
@@ -21,11 +21,19 @@ void aws_string_destroy_secure_harness() {
     struct aws_string *str = ensure_string_is_allocated_bounded_length(MAX_STRING_LEN);
     char const *bytes = str->bytes;
     size_t len = str->len;
-    /* Tell CBMC to keep the buffer live after the free */
-    __CPROVER_allocated_memory(bytes, len);
     bool nondet_parameter;
     aws_string_destroy_secure(nondet_parameter ? str : NULL);
+
+    // Check that all bytes are 0.  Since the memory is freed, this will trigger a use-after-free check
+    // Disabiling the check only for this bit of the harness.
+#pragma CPROVER check push
+#pragma CPROVER check disable "pointer"
     if (nondet_parameter) {
-        assert_all_zeroes(bytes, len);
+        if (len > 0) {
+            size_t i;
+            __CPROVER_assume(i < len);
+            assert(bytes[i] == 0);
+        }
     }
+#pragma CPROVER check pop
 }
