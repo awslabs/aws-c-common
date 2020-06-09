3b7ba27e81426746a9582985642d68e558410394
Jonathan M. Henson <henso@amazon.com>
2020-06-08 17:18:49 -0700

    removed built-in-cpu supports so llvm will quit complaining at link time about __cpu_model

--- cmake/AwsSIMD.cmake
+++ cmake/AwsSIMD.cmake
@@ -56,33 +56,6 @@ int main() {
     return (int)_mm256_extract_epi64(vec, 2);
 }" HAVE_MM256_EXTRACT_EPI64)
 
-check_c_source_compiles("
-#include <immintrin.h>
-
-int main() {
-    return _may_i_use_cpu_feature(_FEATURE_AVX2 | _FEATURE_SSE4_1);
-}
-"   HAVE_MAY_I_USE)
-
-check_c_source_compiles("
-#include <immintrin.h>
-
-int main() {
-    return __builtin_cpu_supports(\"avx2\");
-}
-" HAVE_BUILTIN_CPU_SUPPORTS)
-
-check_c_source_compiles("
-#include <intrin.h>
-
-int main() {
-    int cpuInfo[4] = {0};
-    int function_id = 1;
-    int subfunction_id = 1;
-    __cpuidex(cpuInfo, function_id, subfunction_id);
-    return 0;
-}" HAVE_MSVC_CPUIDEX)
-
 set(CMAKE_REQUIRED_FLAGS "${old_flags}")
 
 macro(simd_add_definition_if target definition)
@@ -95,9 +68,6 @@ endmacro(simd_add_definition_if)
 # Does not set any processor feature codegen flags
 function(simd_add_definitions target)
     simd_add_definition_if(${target} HAVE_AVX2_INTRINSICS)
-    simd_add_definition_if(${target} HAVE_MAY_I_USE)
-    simd_add_definition_if(${target} HAVE_BUILTIN_CPU_SUPPORTS)
-    simd_add_definition_if(${target} HAVE_MSVC_CPUIDEX)
     simd_add_definition_if(${target} HAVE_MM256_EXTRACT_EPI64)
 endfunction(simd_add_definitions)
 
