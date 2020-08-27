929f42e81cb4e700c0a5af004a20be2624e0eb21
Justin Boswell <boswej@amazon.com>
2020-08-27 22:01:37 +0000

    Added AWS_OS_* macros to detect OS

---
+++ include/aws/common/platform.h
@@ -0,0 +1,31 @@
+#ifndef AWS_COMMON_PLATFORM_H
+#define AWS_COMMON_PLATFORM_H
+
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/common/config.h>
+
+#ifdef _WIN32
+#    define AWS_OS_WINDOWS
+#elif __APPLE__
+#    include "TargetConditionals.h"
+#    if defined(TARGET_OS_IPHONE) && defined(TARGET_IPHONE_SIMULATOR)
+#        define AWS_OS_IOS
+#    elif defined(TARGET_OS_IPHONE)
+#        define AWS_OS_IOS
+#    else
+#        define AWS_OS_MACOS
+#        define AWS_OS_POSIX
+#    endif
+#elif __linux__
+#    define AWS_OS_LINUX
+#endif
+
+#if defined(_POSIX_VERSION)
+#    define AWS_OS_POSIX
+#endif
+
+#endif /* AWS_COMMON_PLATFORM_H */
