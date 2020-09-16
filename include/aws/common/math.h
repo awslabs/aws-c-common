13adef72b7813ec878817c6d50a7a3f241015d8a
Justin Boswell <boswej@amazon.com>
2020-09-16 12:12:54 -0700

    Imported PR from #695 to fix aarch64 builds (#696)

--- include/aws/common/math.h
+++ include/aws/common/math.h
@@ -27,8 +27,8 @@
 AWS_EXTERN_C_BEGIN
 
 #if defined(AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS) && (defined(__clang__) || !defined(__cplusplus)) ||                 \
-    defined(__x86_64__) && defined(AWS_HAVE_GCC_INLINE_ASM) || defined(AWS_HAVE_MSVC_MULX) || defined(CBMC) ||         \
-    !defined(AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS)
+    (defined(__x86_64__) || defined(__aarch64__)) && defined(AWS_HAVE_GCC_INLINE_ASM) ||                               \
+    defined(AWS_HAVE_MSVC_MULX) || defined(CBMC) || !defined(AWS_HAVE_GCC_OVERFLOW_MATH_EXTENSIONS)
 /* In all these cases, we can use fast static inline versions of this code */
 #    define AWS_COMMON_MATH_API AWS_STATIC_IMPL
 #else
