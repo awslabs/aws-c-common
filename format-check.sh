89e868576fbcb6ab81fbbf1584203f9cbca7f1e2
Felipe R. Monteiro <felisous@amazon.com>
2020-09-17 22:51:16 -0400

    Improves predicates for aws_array_list proofs (#694)
    * Adds AWS_IMPLIES and AWS_IFF macros
    * Improves the aws_array_list validity function
    * Improves predicates for CBMC proofs
    * Fixes format-check.sh script to scan verification dir
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- format-check.sh
+++ format-check.sh
@@ -10,7 +10,7 @@ if NOT type $CLANG_FORMAT 2> /dev/null ; then
 fi
 
 FAIL=0
-SOURCE_FILES=`find source include tests .cbmc-batch -type f \( -name '*.h' -o -name '*.c' -o -name '*.inl' \)`
+SOURCE_FILES=`find source include tests verification -type f \( -name '*.h' -o -name '*.c' -o -name '*.inl' \)`
 for i in $SOURCE_FILES
 do
     $CLANG_FORMAT -output-replacements-xml $i | grep -c "<replacement " > /dev/null
