d69e2074627c7e4dc058b847a504ffe07c1931c1
Justin Boswell <boswej@amazon.com>
2020-11-13 21:40:26 +0000

    Fixed builder imports

--- .builder/actions/clang-tidy.py
+++ .builder/actions/clang-tidy.py
@@ -1,8 +1,5 @@
 
-from Builder.actions.install import InstallPackages
-from Builder.actions.git import DownloadDependencies
-from Builder.actions.cmake import CMakeBuild
-from Builder.actions.script import Script
+
 import Builder
 import glob
 import os
@@ -18,21 +15,23 @@ def run_clang_tidy(env):
         sys.exit(1)
 
     sources = [os.path.join(env.source_dir, file)
-        for file in glob.glob('source/**/*.c') + glob.glob('source/*.c')
-        if not ('windows' in file or 'android' in file)]
+               for file in glob.glob('source/**/*.c') + glob.glob('source/*.c')
+               if not ('windows' in file or 'android' in file)]
 
-    return Script([
-        [clang_tidy, '-p', os.path.join(env.build_dir, env.project.name)] + sources
+    return Builder.Script([
+        [clang_tidy, '-p',
+            os.path.join(env.build_dir, env.project.name)] + sources
     ])
+
 
 class ClangTidy(Builder.Action):
     def is_main(self):
         return True
 
     def run(self, env):
-        return Script([
-            InstallPackages(['clang-tidy']),
-            DownloadDependencies(),
-            CMakeBuild(env.project),
+        return Builder.Script([
+            Builder.InstallPackages(['clang-tidy']),
+            Builder.DownloadDependencies(),
+            Builder.CMakeBuild(env.project),
             run_clang_tidy,
         ])
