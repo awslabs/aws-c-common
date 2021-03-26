5464be487028fbdca71f80d0a4a83656a018c470
Bret Ambrose <bretambrose@gmail.com>
2021-03-26 13:33:23 -0700

    Disable warning on test (#781)
    * Disable stringop-overflow warning when building tests since sometimes cmake generates code that violates that warning and then downstream consumers get unhappy

--- cmake/AwsTestHarness.cmake
+++ cmake/AwsTestHarness.cmake
@@ -34,6 +34,11 @@ function(generate_test_driver driver_exe_name)
 
     add_executable(${driver_exe_name} ${CMAKE_CURRENT_BINARY_DIR}/test_runner.c ${TESTS})
     aws_set_common_properties(${driver_exe_name} NO_WEXTRA NO_PEDANTIC)
+
+    if (NOT MSVC AND AWS_SHOULD_DISABLE_STRINGOP_OVERFLOW)
+        SET_SOURCE_FILES_PROPERTIES(test_runner.c PROPERTIES COMPILE_FLAGS -Wno-error=stringop-overflow)
+    endif()
+
     aws_add_sanitizers(${driver_exe_name} ${${PROJECT_NAME}_SANITIZERS})
 
     target_link_libraries(${driver_exe_name} PRIVATE ${PROJECT_NAME})
