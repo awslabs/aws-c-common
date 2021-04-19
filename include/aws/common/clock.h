9c71e3b386ad0e51c98d0c1bcd1c7ce7720a23da
Bret Ambrose <bretambrose@gmail.com>
2021-04-19 11:10:50 -0700

    Tidy2

--- include/aws/common/clock.h
+++ include/aws/common/clock.h
@@ -35,7 +35,7 @@ AWS_STATIC_IMPL uint64_t aws_timestamp_convert(
  * More general form of aws_timestamp_convert that takes arbitrary frequencies rather than the timestamp enum.
  */
 AWS_STATIC_IMPL uint64_t
-    aws_timestamp_convert_u64(uint64_t timestamp, uint64_t old_frequency, uint64_t new_frequency, uint64_t *remainder);
+    aws_timestamp_convert_u64(uint64_t ticks, uint64_t old_frequency, uint64_t new_frequency, uint64_t *remainder);
 
 /**
  * Get ticks in nanoseconds (usually 100 nanosecond precision) on the high resolution clock (most-likely TSC). This
