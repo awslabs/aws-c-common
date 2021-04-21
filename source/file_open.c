173dadce79c56d42455ea874f9b52cceb2405eb3
Dengke Tang <dengket@amazon.com>
2021-04-21 11:18:25 -1000

    updates

--- source/file_open.c
+++ source/file_open.c
@@ -5,16 +5,19 @@
 
 #include <aws/common/file_open.h>
 #include <stdio.h>
-#include <windows.h>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
 
 FILE *aws_fopen(const char *file_path, const char *mode) {
 
 #ifdef _WIN32
     wchar_t w_file_path[1000];
 
-    MultiByteToWideChar(0, 0, file_path, -1, w_file_path, (int)(strlen(file_path) + 1));
+    /* the default encoding is utf-8 or ascii */
+    MultiByteToWideChar(CP_UTF8, 0, file_path, -1, w_file_path, (int)(strlen(file_path) + 1));
     wchar_t w_mode[10];
-    MultiByteToWideChar(0, 0, mode, -1, w_mode, (int)(strlen(mode) + 1));
+    MultiByteToWideChar(CP_UTF8, 0, mode, -1, w_mode, (int)(strlen(mode) + 1));
     FILE *file;
     if (_wfopen_s(&file, w_file_path, w_mode)) {
         return NULL;
--- tests/file_open_test.c
+++ tests/file_open_test.c
@@ -3,35 +3,45 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 #include <aws/common/file_open.h>
+#include "fcntl.h"
 
 #include <aws/testing/aws_test_harness.h>
 
-static int s_aws_fopen_unicode_test_fn(struct aws_allocator *allocator, void *ctx) {
-    (void)allocator;
-    (void)ctx;
-
-    wchar_t w_file_path[] = L"C:\\Users\\ting-\\Desktop\\project\\aws-c-common\\Åsample.txt";
-    char I_don[] = "Schöne";
-    char I_don_2[] = "Schone";
-    printf("%s \n", I_don);
-    printf("%d %d \n", (int)strlen(I_don), (int)strlen(I_don_2));
-    // wchar_t file_path[] = L"Åsample.txt";
-    // uint8_t *binary = (uint8_t *)file_path;
-    // printf("%x %x\n", binary[0], binary[1]);
-    // uint8_t temp = binary[1];
-    // binary[1] = binary[0];
-    // binary[0] = temp;
-    // wchar_t *another_path = (wchar_t *)binary;
-    // FILE *file = aws_fopen(file_path, "w+");
-    FILE *file;
-    if (_wfopen_s(&file, w_file_path, L"r")) {
-        return AWS_OP_ERR;
-    }
-    // if (fopen_s(&file, file_path, "w+")) {
-    //     return AWS_OP_ERR;
-    // }
+static int s_aws_fopen_test_helper(char *file_path, char * content) {
+    char read_result [100];
+    FILE *file = aws_fopen(file_path, "w+");
+    ASSERT_NOT_NULL(file);
+    fprintf(file, "%s", content);
     fclose(file);
+    FILE *readfile = aws_fopen(file_path, "r");
+    ASSERT_NOT_NULL(readfile);
+    fread(read_result, sizeof(char), strlen(content), readfile);
+    fclose(readfile);
+    ASSERT_SUCCESS(strcmp(content, read_result));
+    ASSERT_SUCCESS(remove(file_path));
     return AWS_OP_SUCCESS;
 }
 
-AWS_TEST_CASE(aws_fopen_unicode_test, s_aws_fopen_unicode_test_fn)
+static int s_aws_fopen_non_ascii_test_fn(struct aws_allocator *allocator, void *ctx) {
+
+    (void)allocator;
+    (void)ctx;
+    char file_path[] = "Åsample.txt";
+    char content [] = "samples";
+    ASSERT_SUCCESS(s_aws_fopen_test_helper(file_path, content));
+    return AWS_OP_SUCCESS;
+}
+
+AWS_TEST_CASE(aws_fopen_non_ascii_test, s_aws_fopen_non_ascii_test_fn)
+
+static int s_aws_fopen_ascii_test_fn(struct aws_allocator *allocator, void *ctx) {
+
+    (void)allocator;
+    (void)ctx;
+    char file_path[] = "a sample.txt";
+    char content [] = "samples";
+    ASSERT_SUCCESS(s_aws_fopen_test_helper(file_path, content));
+    return AWS_OP_SUCCESS;
+}
+
+AWS_TEST_CASE(aws_fopen_ascii_test, s_aws_fopen_ascii_test_fn)
