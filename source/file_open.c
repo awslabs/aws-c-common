4627670aac73aea0e5797a7fae3af688d88151b5
Dengke Tang <dengket@amazon.com>
2021-04-23 08:26:34 -1000

    Revert "fopen_s instead"
    This reverts commit f7a22b2cf78f714ebfca3bacca9bcb94cfbccbdd.

--- source/file_open.c
+++ source/file_open.c
@@ -12,20 +12,17 @@
 FILE *aws_fopen(const char *file_path, const char *mode) {
 
 #ifdef _WIN32
-    // wchar_t w_file_path[1000];
+    wchar_t w_file_path[1000];
 
-    // /* the default encoding is utf-8 or ascii */
-    // MultiByteToWideChar(CP_UTF8, 0, file_path, -1, w_file_path, (int)(strlen(file_path) + 1));
-    // wchar_t w_mode[10];
-    // MultiByteToWideChar(CP_UTF8, 0, mode, -1, w_mode, (int)(strlen(mode) + 1));
+    /* the default encoding is utf-8 or ascii */
+    MultiByteToWideChar(CP_UTF8, 0, file_path, -1, w_file_path, (int)(strlen(file_path) + 1));
+    wchar_t w_mode[10];
+    MultiByteToWideChar(CP_UTF8, 0, mode, -1, w_mode, (int)(strlen(mode) + 1));
     FILE *file;
-    if (fopen_s(&file, file_path, mode)) {
+    if (_wfopen_s(&file, w_file_path, w_mode)) {
+        /* errno will be set */
         return NULL;
     }
-    // if (_wfopen_s(&file, w_file_path, w_mode)) {
-    //     /* errno will be set */
-    //     return NULL;
-    // }
     return file;
 #else
     return fopen(file_path, mode);
