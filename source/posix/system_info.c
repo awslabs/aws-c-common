448cbd9dce8147f8bbaa541db53d3ed00d6a26e0
Justin Boswell <boswej@amazon.com>
2021-04-30 23:31:27 +0000

    Fixed aws_backtrace_log versions for execinfo/no execinfo

--- source/posix/system_info.c
+++ source/posix/system_info.c
@@ -392,6 +392,21 @@ void aws_backtrace_print(FILE *fp, void *call_site_data) {
     free(symbols);
 }
 
+void aws_backtrace_log(int log_level) {
+    void *stack_frames[AWS_BACKTRACE_DEPTH];
+    size_t num_frames = aws_backtrace(stack_frames, AWS_BACKTRACE_DEPTH);
+    if (!num_frames) {
+        AWS_LOGF(log_level, AWS_LS_COMMON_GENERAL, "Unable to capture backtrace");
+        return;
+    }
+    char **symbols = aws_backtrace_symbols(stack_frames, num_frames);
+    for (size_t line = 0; line < num_frames; ++line) {
+        const char *symbol = symbols[line];
+        AWS_LOGF(log_level, AWS_LS_COMMON_GENERAL, "%s", symbol);
+    }
+    free(symbols);
+}
+
 #else
 void aws_backtrace_print(FILE *fp, void *call_site_data) {
     (void)call_site_data;
@@ -415,22 +430,11 @@ char **aws_backtrace_addr2line(void *const *stack_frames, size_t stack_depth) {
     (void)stack_depth;
     return NULL;
 }
-#endif /* AWS_HAVE_EXECINFO */
 
 void aws_backtrace_log(int log_level) {
-    void *stack_frames[AWS_BACKTRACE_DEPTH];
-    size_t num_frames = aws_backtrace(stack_frames, AWS_BACKTRACE_DEPTH);
-    if (!num_frames) {
-        AWS_LOGF(log_level, AWS_LS_COMMON_GENERAL, "Unable to capture backtrace");
-        return;
-    }
-    char **symbols = aws_backtrace_symbols(stack_frames, num_frames);
-    for (size_t line = 0; line < num_frames; ++line) {
-        const char *symbol = symbols[line];
-        AWS_LOGF(log_level, AWS_LS_COMMON_GENERAL, "%s", symbol);
-    }
-    free(symbols);
+    AWS_LOGF(log_level, AWS_LS_COMMON_GENERAL, "aws_backtrace_log: no execinfo compatible backtrace API available");
 }
+#endif /* AWS_HAVE_EXECINFO */
 
 #if defined(AWS_OS_APPLE)
 enum aws_platform_os aws_get_platform_build_os(void) {
