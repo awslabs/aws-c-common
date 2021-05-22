8223ea2e6da84f2b482c7a519faa71e793efbe99
Justin Boswell <boswej@amazon.com>
2021-05-21 17:49:40 -0700

    Added aws_promise and tests

---
+++ include/aws/common/promise.h
@@ -0,0 +1,79 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#ifndef AWS_COMMON_PROMISE_H
+#define AWS_COMMON_PROMISE_H
+
+#include <aws/common/common.h>
+
+/*
+ * Standard promise interface. Promise can be waited on by multiple threads, and as long as it is
+ * ref-counted correctly, will provide the resultant value/error code to all waiters.
+ * All promise API calls are internally thread-safe.
+ */
+struct aws_promise;
+/*
+ * Creates a new promise
+ */
+struct aws_promise *aws_promise_new(struct aws_allocator *allocator);
+
+/*
+ * Indicate a new reference to a promise. At minimum, each new thread making use of the promise should
+ * acquire it.
+ */
+void aws_promise_acquire(struct aws_promise *promise);
+
+/*
+ * Releases a reference on the promise. When the refcount hits 0, the promise is cleaned up and freed.
+ */
+void aws_promise_release(struct aws_promise *promise);
+
+/*
+ * Waits infinitely for the promise to be completed
+ */
+void aws_promise_wait(struct aws_promise *promise);
+/*
+ * Waits for the requested time in milliseconds. Returns true if the promise was completed.
+ */
+bool aws_promise_wait_for(struct aws_promise *promise, size_t milliseconds);
+
+/*
+ * Completes the promise and stores the result along with an optional destructor. If the value
+ * is not taken via `aws_promise_take_value`, it will be destroyed when the promise's reference
+ * count reaches zero
+ */
+void aws_promise_complete(struct aws_promise *promise, void *value, void (*dtor)(void *));
+
+/*
+ * Completes the promise and stores the error code
+ */
+void aws_promise_fail(struct aws_promise *promise, int error_code);
+
+/*
+ * Returns whether or not the promise has completed (regardless of success or failure)
+ */
+bool aws_promise_is_complete(struct aws_promise *promise);
+
+/*
+ * Returns the error code recorded if the promise failed, or 0 if it succeeded
+ */
+int aws_promise_error_code(struct aws_promise *promise);
+
+/*
+ * Returns the value provided to the promise if it succeeded, or NULL if none was provided
+ * or the promise failed. Check `aws_promise_error_code` to be sure.
+ * NOTE: The ownership of the value is retained by the promise.
+ */
+void *aws_promise_value(struct aws_promise *promise);
+
+/*
+ * Returns the value provided to the promise if it succeeded, or NULL if none was provided
+ * or the promise failed. Check `aws_promise_error_code` to be sure.
+ * NOTE: The promise relinquishes ownership of the value, the caller is now responsible for
+ * freeing any resources associated with the value
+ */
+void *aws_promise_take_value(struct aws_promise *promise);
+
+#endif //AWS_COMMON_PROMISE_H
