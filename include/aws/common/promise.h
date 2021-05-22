79cb777eadf85e94e6348cfbcd9594a04f1e892f
Justin Boswell <boswej@amazon.com>
2021-05-21 18:06:17 -0700

    Fixed visibility on promise API

--- include/aws/common/promise.h
+++ include/aws/common/promise.h
@@ -17,26 +17,31 @@ struct aws_promise;
 /*
  * Creates a new promise
  */
+AWS_COMMON_API
 struct aws_promise *aws_promise_new(struct aws_allocator *allocator);
 
 /*
  * Indicate a new reference to a promise. At minimum, each new thread making use of the promise should
  * acquire it.
  */
+AWS_COMMON_API
 void aws_promise_acquire(struct aws_promise *promise);
 
 /*
  * Releases a reference on the promise. When the refcount hits 0, the promise is cleaned up and freed.
  */
+AWS_COMMON_API
 void aws_promise_release(struct aws_promise *promise);
 
 /*
  * Waits infinitely for the promise to be completed
  */
+AWS_COMMON_API
 void aws_promise_wait(struct aws_promise *promise);
 /*
  * Waits for the requested time in milliseconds. Returns true if the promise was completed.
  */
+AWS_COMMON_API
 bool aws_promise_wait_for(struct aws_promise *promise, size_t milliseconds);
 
 /*
@@ -44,21 +49,25 @@ bool aws_promise_wait_for(struct aws_promise *promise, size_t milliseconds);
  * is not taken via `aws_promise_take_value`, it will be destroyed when the promise's reference
  * count reaches zero
  */
+AWS_COMMON_API
 void aws_promise_complete(struct aws_promise *promise, void *value, void (*dtor)(void *));
 
 /*
  * Completes the promise and stores the error code
  */
+AWS_COMMON_API
 void aws_promise_fail(struct aws_promise *promise, int error_code);
 
 /*
  * Returns whether or not the promise has completed (regardless of success or failure)
  */
+AWS_COMMON_API
 bool aws_promise_is_complete(struct aws_promise *promise);
 
 /*
  * Returns the error code recorded if the promise failed, or 0 if it succeeded
  */
+AWS_COMMON_API
 int aws_promise_error_code(struct aws_promise *promise);
 
 /*
@@ -66,6 +75,7 @@ int aws_promise_error_code(struct aws_promise *promise);
  * or the promise failed. Check `aws_promise_error_code` to be sure.
  * NOTE: The ownership of the value is retained by the promise.
  */
+AWS_COMMON_API
 void *aws_promise_value(struct aws_promise *promise);
 
 /*
@@ -74,6 +84,7 @@ void *aws_promise_value(struct aws_promise *promise);
  * NOTE: The promise relinquishes ownership of the value, the caller is now responsible for
  * freeing any resources associated with the value
  */
+AWS_COMMON_API
 void *aws_promise_take_value(struct aws_promise *promise);
 
 #endif // AWS_COMMON_PROMISE_H
