6e6f88b3a6c34a4c3917457b6373605635e0595d
Jonathan M. Henson <henso@amazon.com>
2021-06-21 15:07:17 -0700

    Lets work on linking?

--- aws_crt_common_ws/aws-crt-c-flags/src/lib.rs
+++ aws_crt_common_ws/aws-crt-c-flags/src/lib.rs
@@ -486,6 +486,10 @@ impl CRTModuleBuildInfo {
             for include in &module.public_include_dirs {
                 self.build_toolchain.include(include);
             }
+
+            for lib in &module.link_targets {
+                self.link_targets.push(lib.clone());
+            }
         }
 
         if self.shared_lib {
