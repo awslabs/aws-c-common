fcf5c9e53fd4bf6215070ade1fe7efbb888cded0
Bret Ambrose <bretambrose@gmail.com>
2021-08-19 10:25:47 -0700

    Final warning

--- tests/byte_buf_test.c
+++ tests/byte_buf_test.c
@@ -225,6 +225,7 @@ static int s_test_buffer_advance(struct aws_allocator *allocator, void *ctx) {
     (void)allocator;
 
     uint8_t arr[16];
+    AWS_ZERO_ARRAY(arr);
     struct aws_byte_buf src_buf = aws_byte_buf_from_empty_array(arr, sizeof(arr));
 
     struct aws_byte_buf dst_buf = {0};
