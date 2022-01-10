6c5ca05651dc4c587cfc39679ed2c46f3374d673
Dengke Tang <dengket@amazon.com>
2022-01-10 13:31:53 -0800

    missing a star

--- source/random_access_set.c
+++ source/random_access_set.c
@@ -107,7 +107,7 @@ int aws_random_access_set_remove(struct aws_random_access_set *set, const void *
         void *last_element = NULL;
         AWS_FATAL_ASSERT(
             aws_array_list_get_at_ptr(&set->impl->list, &last_element, current_length - 1) == AWS_OP_SUCCESS);
-        struct aws_string *str = *(struct aws_string **)last_element;
+        struct aws_string *str = **(struct aws_string ***)last_element;
         AWS_LOGF_ERROR(AWS_LS_COMMON_GENERAL, "last element: %d %s\n", (int)str->len, aws_string_c_str(str));
         str = *(struct aws_string **)element;
         AWS_LOGF_ERROR(
