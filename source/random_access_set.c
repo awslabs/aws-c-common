3717931782fec8ad90953a14a5ea23740240d1e6
Dengke Tang <dengket@amazon.com>
2022-01-10 13:47:52 -0800

    great, I don't understand, but it seems working

--- source/random_access_set.c
+++ source/random_access_set.c
@@ -7,9 +7,6 @@
 #include <aws/common/allocator.h>
 #include <aws/common/device_random.h>
 #include <aws/common/random_access_set.h>
-#include <aws/common/string.h>
-
-#include <aws/common/logging.h>
 
 struct aws_random_access_set_impl {
     struct aws_allocator *allocator;
@@ -82,8 +79,6 @@ int aws_random_access_set_insert(struct aws_random_access_set *set, const void *
     if (aws_array_list_push_back(&set->impl->list, (void *)&element)) {
         goto list_push_error;
     }
-    // AWS_LOGF_ERROR(AWS_LS_COMMON_GENERAL, " index for it: %zu", current_length);
-    // (void)current_length;
     if (aws_hash_table_put(&set->impl->map, element, (void *)(aws_array_list_length(&set->impl->list) - 1), NULL)) {
         goto error;
     }
@@ -102,16 +97,6 @@ int aws_random_access_set_remove(struct aws_random_access_set *set, const void *
         /* Nothing to remove */
         return AWS_OP_SUCCESS;
     }
-    {
-        void *last_element = NULL;
-        AWS_FATAL_ASSERT(
-            aws_array_list_get_at_ptr(&set->impl->list, &last_element, current_length - 1) == AWS_OP_SUCCESS);
-        struct aws_string *str = **(struct aws_string ***)last_element;
-        AWS_LOGF_ERROR(AWS_LS_COMMON_GENERAL, "last element: %d %s\n", (int)str->len, aws_string_c_str(str));
-        str = *(struct aws_string **)element;
-        AWS_LOGF_ERROR(
-            AWS_LS_COMMON_GENERAL, "current element to remove: %d %s\n", (int)str->len, aws_string_c_str(str));
-    }
     struct aws_hash_element *find = NULL;
     /* find and remove the element from table */
     if (aws_hash_table_find(&set->impl->map, element, &find)) {
@@ -126,8 +111,6 @@ int aws_random_access_set_remove(struct aws_random_access_set *set, const void *
     if (aws_hash_table_remove_element(&set->impl->map, find)) {
         return AWS_OP_ERR;
     }
-    AWS_LOGF_ERROR(
-        AWS_LS_COMMON_GENERAL, "index to remove: %d, current length: %d\n", (int)index_to_remove, (int)current_length);
     /* Nothing else can fail after here. */
     if (index_to_remove != current_length - 1) {
         /* It's not the last element, we need to swap it with the end of the list and remove the last element */
