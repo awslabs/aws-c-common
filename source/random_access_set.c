07b34c7bd10f216724013d0194b53c2fed35e7e3
Dengke Tang <dengket@amazon.com>
2022-01-10 13:36:49 -0800

    it will fail

--- source/random_access_set.c
+++ source/random_access_set.c
@@ -83,7 +83,7 @@ int aws_random_access_set_insert(struct aws_random_access_set *set, const void *
     if (aws_array_list_push_back(&set->impl->list, (void *)&element)) {
         goto list_push_error;
     }
-    AWS_LOGF_ERROR(AWS_LS_COMMON_GENERAL, " index for it: %zu", current_length);
+    // AWS_LOGF_ERROR(AWS_LS_COMMON_GENERAL, " index for it: %zu", current_length);
     // (void)current_length;
     if (aws_hash_table_put(&set->impl->map, element, (void *)current_length, NULL)) {
         goto error;
