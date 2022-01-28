90445db871ff765e428a1bdb852939bb820c3cd7
Jérémy Coulon <jeremy.coulon@3ds.com>
2022-01-28 17:05:43 +0100

    Fix macro AWS_SUPPRESS_ASAN for gcc

--- include/aws/common/macros.h
+++ include/aws/common/macros.h
@@ -75,13 +75,9 @@ AWS_STATIC_ASSERT(CALL_OVERLOAD_TEST(1, 2, 3) == 3);
 #    endif     /*  defined(__GNUC__) || defined(__clang__) */
 #endif         /*  defined(_MSC_VER) */
 
-#if defined(__has_feature)
-#    if __has_feature(address_sanitizer)
-#        define AWS_SUPPRESS_ASAN __attribute__((no_sanitize("address")))
-#    endif
-#endif
-
-#if !defined(AWS_SUPPRESS_ASAN)
+#if defined(__clang__) || defined (__GNUC__)
+#    define AWS_SUPPRESS_ASAN __attribute__((no_sanitize_address))
+#else
 #    define AWS_SUPPRESS_ASAN
 #endif
 
