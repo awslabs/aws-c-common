83693832495a9545dddeaa9504a05dc171e79eca
Noah Beard <ncbeard@amazon.com>
2022-03-02 10:30:14 -0500

    Adjusted JSON module to fit code style, added code documentation

--- include/aws/common/json/json.h
+++ include/aws/common/json/json.h
@@ -9,16 +9,27 @@
 #include <aws/common/json/external/cJSON.h>
 #include <aws/common/thread.h>
 
-struct aws_allocator *s_json_module_allocator;
-bool s_json_module_initialized;
+struct aws_allocator *aws_json_module_allocator;
+bool aws_json_module_initialized;
 
-void *s_cJSON_alloc(size_t sz);
-void s_cJSON_free(void *ptr);
-void s_json_init(struct aws_allocator *allocator);
-void s_json_clean_up(void);
+void *aws_cJSON_alloc(size_t sz);
+void aws_cJSON_free(void *ptr);
 
+/**
+ * Call to initialize the JSON module prior to calling "aws_json_*" functions.
+ * @param allocator The allocator used to create and free cJSON objects.
+ */
+void aws_json_init(struct aws_allocator *allocator);
+/**
+ * Call when finished with the JSON module to clean up JSON memory.
+ */
+void aws_json_clean_up(void);
 
-struct aws_json_parse_credentials_from_json_doc_options {
+/**
+ * The options that can be passed to the aws_json_parse_credentials_from_cjson function.
+ * Required for the aws_json_parse_credentials_from_cjson function.
+ */
+struct aws_json_parse_credentials_options {
     const char *access_key_id_name;
     const char *secrete_access_key_name;
     const char *token_name;
@@ -26,44 +37,187 @@ struct aws_json_parse_credentials_from_json_doc_options {
     bool token_required;
     bool expiration_required;
 };
-struct aws_json_parse_credentials_from_json_doc_results {
+/**
+ * All of the credentials extracted from the cJSON document root passed into the
+ * aws_json_parse_credentials_from_cjson function.
+ */
+struct aws_json_parse_credentials_results {
     char *access_key_id;
     char *secret_access_key;
     char *token;
     char *creds_expiration;
     uint64_t expiration_timepoint_in_seconds;
 };
-struct aws_json_parse_credentials_from_json_doc_results s_json_parse_credentials_from_cjson_object(
+/**
+ * Parses a cJSON object (expected document root) for credentials. Used to correctly parse and extract response
+ * data from JSON documents sent by AWS servers.
+ * @param document_root The cJSON document root
+ * @param options The options to use when parsing the document root
+ * @return aws_json_parse_credentials_result struct containing extracted credentials
+ */
+struct aws_json_parse_credentials_results aws_json_parse_credentials_from_cjson(
     struct cJSON *document_root,
-    const struct aws_json_parse_credentials_from_json_doc_options *options);
+    const struct aws_json_parse_credentials_options *options);
 
-cJSON *m_get_cjson_if_valid (cJSON *root, const char* name, const char* logName);
+/**
+ * Creates a new cJSON object and returns a pointer to it
+ * @return A pointer to a new cJSON object
+ */
+struct cJSON *aws_json_create_cjson(void);
+/**
+ * Creates a new cJSON object, validates its creation, and adds it to the passed-in cJSON object.
+ * Use the "output" pointer argument to get a reference to the newly created cJSON object.
+ * @param object The cJSON object you want to add the new cJSON object to.
+ * @param item_key The key to place the new cJSON object at.
+ * @param output The newly created cJSON object.
+ * @return Returns true if creation and addition was successful.
+ */
+bool aws_json_create_and_add_cjson(cJSON *object, const char* item_key, cJSON* output);
+/**
+ * Creates a new cJSON array and returns a pointer to it
+ * @return A pointer to a new cJSON array
+ */
+struct cJSON *aws_json_create_cjson_array(void);
+/**
+ * Creates a new cJSON array, validates its creation, and adds it to the passed-in cJSON object.
+ * Use the "output" pointer argument to get a reference to the newly created cJSON array.
+ * @param object The cJSON object you want to add the new cJSON array to.
+ * @param item_key The key to place the new cJSON array at.
+ * @param output The newly created cJSON array.
+ * @return Returns true if creation and addition was successful.
+ */
+bool aws_json_create_and_add_cjson_array(cJSON *object, const char* item_key, cJSON* output);
+/**
+ * Creates a new cJSON number object and returns a pointer to it.
+ * @param item_value The number to be stored in the cJSON number object.
+ * @return A pointer to the new cJSON number object.
+ */
+cJSON* aws_json_create_cjson_number(const double item_value);
+/**
+ * Creates a new cJSON string object and returns a pointer to it.
+ * @param item_value The string to be stored in the cJSON string object.
+ * @return A pointer to the new cJSON string object.
+ */
+cJSON* aws_json_create_cjson_string(const char* item_value);
 
+/**
+ * Parses a string containing a JSON document and returns the root JSON element as a cJSON object.
+ * @param string The string containing the JSON document.
+ * @return A pointer to the cJSON object created from the JSON document, or null if parsing fails.
+ */
+cJSON* aws_json_parse_cjson_from_string(const char* string);
 
-struct cJSON *m_create_cjson(void);
-void m_add_item_to_cjson(cJSON *object, const char* item_key, cJSON *item_value);
-bool m_create_and_add_cjson(cJSON *object, const char* item_key, cJSON* output);
-bool m_add_string_to_cjson(cJSON *object, const char* item_key, const char* item_value);
-bool m_add_number_to_cjson(cJSON *object, const char* item_key, const double item_value);
-struct cJSON *m_create_cjson_array(void);
-bool m_create_and_add_cjson_array(cJSON *object, const char* item_key, cJSON* output);
-void m_add_item_to_cjson_array(cJSON *array, cJSON* item_value);
-cJSON* m_create_cjson_number(const double item_value);
-cJSON* m_create_cjson_string(const char* item_value);
-char* m_print_unformatted_cjson(cJSON *object);
-void m_delete_cjson(cJSON *object);
-void m_free_cjson(cJSON *object);
+/**
+ * Adds a cJSON object (item_value) to another cJSON object (object) at the given key (item_key).
+ * @param object The cJSON object you want to add a cJSON object to.
+ * @param item_key The key name you want to place the cJSON object at.
+ * @param item_value The cJSON object you want to add.
+ */
+void aws_json_add_item_to_cjson(cJSON *object, const char* item_key, cJSON *item_value);
+/**
+ * Adds a cJSON object (item_value) to a cJSON array (object) at the end of the cJSON array.
+ * @param array The cJSON array you want to add a cJSON object to.
+ * @param item_value The cJSON object you want to add.
+ */
+void aws_json_add_item_to_cjson_array(cJSON *array, cJSON* item_value);
+/**
+ * Adds a cJSON string to the cJSON object at the given key.
+ * @param object The cJSON object you want to add a cJSON string to.
+ * @param item_key The key name you want to place the cJSON sting at.
+ * @param item_value The string you want to add.
+ * @return True if adding the cJSON string is successful, false otherwise.
+ */
+bool aws_json_add_string_to_cjson(cJSON *object, const char* item_key, const char* item_value);
+/**
+ * Adds a cJSON number to the cJSON object at the given key.
+ * @param object The cJSON object you want to add a cJSON number to.
+ * @param item_key The key name you want to place the cJSON number at.
+ * @param item_value The number you want to add.
+ * @return True if adding the cJSON number is successful, false otherwise.
+ */
+bool aws_json_add_number_to_cjson(cJSON *object, const char* item_key, const double item_value);
 
-cJSON* m_parse_cjson(const char* object);
-cJSON* m_get_cjson_case_sensitive(cJSON *object, const char* item_key);
-cJSON* m_get_cjson(cJSON *object, const char* item_key);
-char* m_get_cjson_string(cJSON *object);
-cJSON* m_get_cjson_array_item(cJSON *object, int index);
+/**
+ * Returns the cJSON item in the passed-in cJSON item at the given key.
+ * @param object The cJSON item that contains the cJSON item at the key.
+ * @param item_key The key name where the cJSON item is placed.
+ * @return A pointer to the cJSON item at the given key if it exists, otherwise null.
+ */
+cJSON* aws_json_get_cjson(cJSON *object, const char* item_key);
+/**
+ * Returns the cJSON item in the passed-in cJSON item at the given key. Will validate the existence of
+ * the cJSON key and will print a log error if the cJSON item does not exist.
+ * @param object The cJSON item that contains the cJSON item at the key.
+ * @param item_key The key name where the cJSON is placed.
+ * @param log_key_name The name to show in the error log if the key does not exist in the cJSON item.
+ * @return A pointer to the cJSON item at the given key if it exists, otherwise null.
+ */
+cJSON *aws_json_get_cjson_with_validate(cJSON *object, const char* item_key, const char* log_key_name);
+/**
+ * Returns the cJSON item in the passed-in cJSON item at the given case-sensitive key.
+ * @param object The cJSON item that contains the cJSON item at the key.
+ * @param item_key The case sensitive key name where teh cJSON item is placed.
+ * @return A pointer to the cJSON item at the given key if it exists, otherwise null.
+ */
+cJSON* aws_json_get_cjson_case_sensitive(cJSON *object, const char* item_key);
+/**
+ * Gets the string value from the cJSON object and returns it.
+ * @param object The cJSON object containing the string value.
+ * @return The string value in the cJSON object.
+ */
+char* aws_json_get_cjson_string(cJSON *object);
+/**
+ * Returns the cJSON object at the given array index, if it exists.
+ * @param object The cJSON array.
+ * @param index The index in the cJSON array.
+ * @return A pointer to the cJSON item at the given index if it exists, otherwise null.
+ */
+cJSON* aws_json_get_cjson_array_item(cJSON *object, int index);
 
-bool m_is_cjson_object(cJSON* object);
-bool m_is_cjson_number(cJSON* object);
-bool m_is_cjson_array(cJSON* object);
+/**
+ * Checks whether the cJSON pointer is a cJSON object.
+ * @param object The cJSON pointer to check.
+ * @return True if the cJSON pointer is a cJSON object, otherwise false.
+ */
+bool aws_json_is_cjson_object(cJSON* object);
+/**
+ * Checks whether the cJSON pointer is a cJSON number.
+ * @param object The cJSON pointer to check.
+ * @return True if the cJSON pointer is a cJSON number, otherwise false.
+ */
+bool aws_json_is_cjson_number(cJSON* object);
+/**
+ * Checks whether the cJSON pointer is a cJSON array.
+ * @param object The cJSON pointer to check.
+ * @return True if the cJSON pointer is a cJSON array, otherwise false.
+ */
+bool aws_json_is_cjson_array(cJSON* object);
 
-void m_print_preallocated_cjson(cJSON* object, char* output, int length, int fmt);
+/**
+ * Returns a string containing the unformatted print of a cJSON object. Can be used to get the JSON string from a
+ * cJSON document root.
+ * @param object The cJSON object to print.
+ * @return An unformatted JSON string of the cJSON object.
+ */
+char*aws_json_print_unformatted_cjson(cJSON *object);
+/**
+ * Stores a string containing the unformatted print of a cJSON object into the given string as output.
+ * @param object The cJSON object to print.
+ * @param output The string to store the print output in.
+ * @param length The length of the string.
+ * @param fmt Whether to format the output (0 = false, 1 = true).
+ */
+void aws_json_print_preallocated_cjson(cJSON* object, char* output, int length, int fmt);
+
+/**
+ * Deletes the cJSON object
+ * @param object The cJSON object to delete.
+ */
+void aws_json_delete_cjson(cJSON *object);
+/**
+ * Frees the cJSON object.
+ * @param object The cJSON object to free.
+ */
+void aws_json_free_cjson(cJSON *object);
 
 #endif // AWS_COMMON_JSON_H
--- source/json/json.c
+++ source/json/json.c
@@ -10,39 +10,39 @@
 #include <aws/common/date_time.h>
 
 
-struct aws_allocator *s_json_module_allocator = NULL;
-bool s_json_module_initialized = false;
+struct aws_allocator *aws_json_module_allocator = NULL;
+bool aws_json_module_initialized = false;
 
-void *s_cJSON_alloc(size_t sz) {
-    return aws_mem_acquire(s_json_module_allocator, sz);
+void *aws_cJSON_alloc(size_t sz) {
+    return aws_mem_acquire(aws_json_module_allocator, sz);
 }
 
-void s_cJSON_free(void *ptr) {
-    aws_mem_release(s_json_module_allocator, ptr);
+void aws_cJSON_free(void *ptr) {
+    aws_mem_release(aws_json_module_allocator, ptr);
 }
 
-void s_json_init(struct aws_allocator *allocator) {
-    if (!s_json_module_initialized)
+void aws_json_init(struct aws_allocator *allocator) {
+    if (!aws_json_module_initialized)
     {
-        s_json_module_allocator = allocator;
-        struct cJSON_Hooks allocation_hooks = {.malloc_fn = s_cJSON_alloc, .free_fn = s_cJSON_free};
+        aws_json_module_allocator = allocator;
+        struct cJSON_Hooks allocation_hooks = {.malloc_fn = aws_cJSON_alloc, .free_fn = aws_cJSON_free};
         cJSON_InitHooks(&allocation_hooks);
-        s_json_module_initialized = true;
+        aws_json_module_initialized = true;
     }
 }
 
-void s_json_clean_up()
+void aws_json_clean_up()
 {
-    if (s_json_module_initialized)
+    if (aws_json_module_initialized)
     {
-        s_json_module_allocator = NULL;
-        s_json_module_initialized = false;
+        aws_json_module_allocator = NULL;
+        aws_json_module_initialized = false;
     }
 }
 
-struct aws_json_parse_credentials_from_json_doc_results s_json_parse_credentials_from_cjson_object(
+struct aws_json_parse_credentials_results aws_json_parse_credentials_from_cjson(
     struct cJSON *document_root,
-    const struct aws_json_parse_credentials_from_json_doc_options *options) {
+    const struct aws_json_parse_credentials_options *options) {
 
     AWS_FATAL_ASSERT(document_root);
     AWS_FATAL_ASSERT(options);
@@ -56,7 +56,7 @@ struct aws_json_parse_credentials_from_json_doc_results s_json_parse_credentials
         AWS_FATAL_ASSERT(options->expiration_name);
     }
 
-    struct aws_json_parse_credentials_from_json_doc_results return_val;
+    struct aws_json_parse_credentials_results return_val;
     return_val.access_key_id = NULL;
     return_val.secret_access_key = NULL;
     return_val.token = NULL;
@@ -73,20 +73,21 @@ struct aws_json_parse_credentials_from_json_doc_results s_json_parse_credentials
     // Pull out the credentials components
     //
 
-    access_key_id = m_get_cjson_if_valid(document_root, options->access_key_id_name, "AccessKeyId");
+    access_key_id = aws_json_get_cjson_with_validate(document_root, options->access_key_id_name, "AccessKeyId");
     if (access_key_id == NULL) {
         goto done;
     }
     return_val.access_key_id = access_key_id->valuestring;
 
-    secrete_access_key = m_get_cjson_if_valid(document_root, options->secrete_access_key_name, "SecretAccessKey");
+    secrete_access_key =
+        aws_json_get_cjson_with_validate(document_root, options->secrete_access_key_name, "SecretAccessKey");
     if (secrete_access_key == NULL) {
         goto done;
     }
     return_val.secret_access_key = secrete_access_key->valuestring;
 
     if (options->token_name) {
-        token = m_get_cjson_if_valid(document_root, options->token_name, "Token");
+        token = aws_json_get_cjson_with_validate(document_root, options->token_name, "Token");
         if (token == NULL) {
             goto done;
         }
@@ -94,7 +95,7 @@ struct aws_json_parse_credentials_from_json_doc_results s_json_parse_credentials
     }
 
     if (options->expiration_name) {
-        creds_expiration = m_get_cjson_if_valid(document_root, options->expiration_name, "Expiration");
+        creds_expiration = aws_json_get_cjson_with_validate(document_root, options->expiration_name, "Expiration");
         if (creds_expiration == NULL) {
             goto done;
         }
@@ -132,39 +133,68 @@ done:
     return return_val;
 }
 
-cJSON *m_get_cjson_if_valid(cJSON *root, const char* name, const char* logName)
-{
-    cJSON *returnVal = cJSON_GetObjectItem(root, name);
-    if (!cJSON_IsString(returnVal) || (returnVal->valuestring == NULL)) {
-        AWS_LOGF_ERROR(AWS_OP_ERR, "Failed to parse from Json document: %s", logName);
-        return NULL;
-    }
-    return returnVal;
-}
-
-
-
-struct cJSON *m_create_cjson()
+struct cJSON* aws_json_create_cjson()
 {
     return cJSON_CreateObject();
 }
-void m_add_item_to_cjson(cJSON *object, const char* item_key, cJSON *item_value)
-{
-    cJSON_AddItemToObject(object, item_key, item_value);
-}
 
-bool m_create_and_add_cjson(cJSON *object, const char* item_key, cJSON *output)
+bool aws_json_create_and_add_cjson(cJSON *object, const char* item_key, cJSON *output)
 {
-    output = m_create_cjson();
+    output = aws_json_create_cjson();
     if (output == NULL)
     {
         return false;
     }
-    m_add_item_to_cjson(object, item_key, output);
+    aws_json_add_item_to_cjson(object, item_key, output);
     return true;
 }
 
-bool m_add_string_to_cjson(cJSON *object, const char* item_key, const char* item_value)
+struct cJSON* aws_json_create_cjson_array()
+{
+    return cJSON_CreateArray();
+}
+
+bool aws_json_create_and_add_cjson_array(cJSON *object, const char* item_key, cJSON *output)
+{
+    output = aws_json_create_cjson_array();
+    if (output == NULL)
+    {
+        return false;
+    }
+    aws_json_add_item_to_cjson(object, item_key, output);
+    return true;
+}
+
+cJSON* aws_json_create_cjson_number(const double item_value)
+{
+    return cJSON_CreateNumber(item_value);
+}
+cJSON* aws_json_create_cjson_string(const char* item_value)
+{
+    return cJSON_CreateString(item_value);
+}
+
+cJSON* aws_json_parse_cjson_from_string(const char* string)
+{
+    cJSON *object = cJSON_Parse(string);
+    if (object == NULL) {
+        AWS_LOGF_ERROR(AWS_OP_ERR, "Failed to parse document as Json document.");
+        return NULL;
+    }
+    return object;
+}
+
+void aws_json_add_item_to_cjson(cJSON *object, const char* item_key, cJSON *item_value)
+{
+    cJSON_AddItemToObject(object, item_key, item_value);
+}
+
+void aws_json_add_item_to_cjson_array(cJSON *array, cJSON* item_value)
+{
+    cJSON_AddItemToArray(array, item_value);
+}
+
+bool aws_json_add_string_to_cjson(cJSON *object, const char* item_key, const char* item_value)
 {
     if (cJSON_AddStringToObject(object, item_key, item_value) == NULL)
     {
@@ -172,7 +202,8 @@ bool m_add_string_to_cjson(cJSON *object, const char* item_key, const char* item
     }
     return true;
 }
-bool m_add_number_to_cjson(cJSON *object, const char* item_key, const double item_value)
+
+bool aws_json_add_number_to_cjson(cJSON *object, const char* item_key, const double item_value)
 {
     if (cJSON_AddNumberToObject(object, item_key, item_value) == NULL)
     {
@@ -181,91 +212,67 @@ bool m_add_number_to_cjson(cJSON *object, const char* item_key, const double ite
     return true;
 }
 
-struct cJSON *m_create_cjson_array()
+cJSON* aws_json_get_cjson(cJSON *object, const char* item_key)
 {
-    return cJSON_CreateArray();
-}
-bool m_create_and_add_cjson_array(cJSON *object, const char* item_key, cJSON *output)
-{
-    output = m_create_cjson_array();
-    if (output == NULL)
-    {
-        return false;
-    }
-    m_add_item_to_cjson(object, item_key, output);
-    return true;
+    return cJSON_GetObjectItem(object, item_key);
 }
 
-void m_add_item_to_cjson_array(cJSON *array, cJSON* item_value)
+cJSON* aws_json_get_cjson_with_validate(cJSON *object, const char* item_key, const char* log_key_name)
 {
-    cJSON_AddItemToArray(array, item_value);
-}
-
-cJSON* m_create_cjson_number(const double item_value)
-{
-    return cJSON_CreateNumber(item_value);
-}
-cJSON* m_create_cjson_string(const char* item_value)
-{
-    return cJSON_CreateString(item_value);
-}
-
-char* m_print_unformatted_cjson(cJSON *object)
-{
-    return cJSON_PrintUnformatted(object);
-}
-
-void m_delete_cjson(cJSON *object)
-{
-    cJSON_Delete(object);
-}
-void m_free_cjson(cJSON *object)
-{
-    cJSON_free(object);
-}
-
-cJSON* m_parse_cjson(const char* path)
-{
-    cJSON *object = cJSON_Parse(path);
-    if (object == NULL) {
-        AWS_LOGF_ERROR(AWS_OP_ERR, "Failed to parse document as Json document.");
+    cJSON *return_val = aws_json_get_cjson(object, item_key);
+    if (!cJSON_IsString(return_val) || (return_val->valuestring == NULL)) {
+        AWS_LOGF_ERROR(AWS_OP_ERR, "Failed to parse from Json document: %s", log_key_name);
         return NULL;
     }
-    return object;
+    return return_val;
 }
 
-cJSON* m_get_cjson_case_sensitive(cJSON *object, const char* item_key)
+cJSON*aws_json_get_cjson_case_sensitive(cJSON *object, const char* item_key)
 {
     return cJSON_GetObjectItemCaseSensitive(object, item_key);
 }
 
-cJSON* m_get_cjson(cJSON *object, const char* item_key)
-{
-    return cJSON_GetObjectItem(object, item_key);
-}
-char* m_get_cjson_string(cJSON *object)
+char*aws_json_get_cjson_string(cJSON *object)
 {
     return cJSON_GetStringValue(object);
 }
-cJSON* m_get_cjson_array_item(cJSON *object, int index)
+
+cJSON*aws_json_get_cjson_array_item(cJSON *object, int index)
 {
     return cJSON_GetArrayItem(object, index);
 }
 
-bool m_is_cjson_object(cJSON* object)
+bool aws_json_is_cjson_object(cJSON* object)
 {
     return cJSON_IsObject(object);
 }
-bool m_is_cjson_number(cJSON* object)
+
+bool aws_json_is_cjson_number(cJSON* object)
 {
     return cJSON_IsNumber(object);
 }
-bool m_is_cjson_array(cJSON* object)
+
+bool aws_json_is_cjson_array(cJSON* object)
 {
     return cJSON_IsArray(object);
 }
 
-void m_print_preallocated_cjson(cJSON* object, char* output, int length, int fmt)
+char*aws_json_print_unformatted_cjson(cJSON *object)
+{
+    return cJSON_PrintUnformatted(object);
+}
+
+void aws_json_print_preallocated_cjson(cJSON* object, char* output, int length, int fmt)
 {
     cJSON_PrintPreallocated(object, output, length, fmt);
+}
+
+void aws_json_delete_cjson(cJSON *object)
+{
+    cJSON_Delete(object);
+}
+
+void aws_json_free_cjson(cJSON *object)
+{
+    cJSON_free(object);
 }
