5dee042581458dbb21a4c139d81f5eb655a0fa05
Noah Beard <ncbeard@amazon.com>
2022-03-07 15:12:50 -0500

    Fixed JSON tests

--- tests/json_test.c
+++ tests/json_test.c
@@ -17,43 +17,43 @@ static int s_test_json_parse_from_string(struct aws_allocator *allocator, void *
     struct aws_json_node *root = aws_json_from_string(s_test_json);
 
     ASSERT_NOT_NULL(root);
-    AWS_ASSERT(aws_json_is_object(root));
+    ASSERT_INT_EQUALS(aws_json_is_object(root), AWS_OP_SUCCESS);
 
     // Testing valid array
     struct aws_json_node* array_node = aws_json_object_get(root, "array");
     ASSERT_NOT_NULL(array_node);
-    ASSERT_TRUE(aws_json_is_array(array_node));
+    ASSERT_INT_EQUALS(aws_json_is_array(array_node), AWS_OP_SUCCESS);
     ASSERT_TRUE(aws_json_array_get_count(array_node) == 3);
     struct aws_json_node* array_node_one = aws_json_array_get(array_node, 0);
     ASSERT_NOT_NULL(array_node_one);
-    ASSERT_TRUE(aws_json_is_number(array_node_one));
+    ASSERT_INT_EQUALS(aws_json_is_number(array_node_one), AWS_OP_SUCCESS);
     ASSERT_TRUE(*aws_json_number_get(array_node_one) == (double)1);
 
     // Testing valid boolean
     struct aws_json_node* boolean_node = aws_json_object_get(root, "boolean");
     ASSERT_NOT_NULL(boolean_node);
-    ASSERT_TRUE(aws_json_is_boolean(boolean_node));
+    ASSERT_INT_EQUALS(aws_json_is_boolean(boolean_node), AWS_OP_SUCCESS);
     ASSERT_TRUE(aws_json_boolean_get(boolean_node));
 
     // Testing valid string
     struct aws_json_node* string_node = aws_json_object_get(root, "color");
     ASSERT_NOT_NULL(string_node);
-    ASSERT_TRUE(aws_json_is_string(string_node));
+    ASSERT_INT_EQUALS(aws_json_is_string(string_node), AWS_OP_SUCCESS);
     ASSERT_TRUE(strcmp(aws_json_string_get(string_node), "gold") == 0);
 
     // Testing valid number
     struct aws_json_node *number_node = aws_json_object_get(root, "number");
     ASSERT_NOT_NULL(number_node);
-    ASSERT_TRUE(aws_json_is_number(number_node));
+    ASSERT_INT_EQUALS(aws_json_is_number(number_node), AWS_OP_SUCCESS);
     ASSERT_TRUE(*aws_json_number_get(number_node) == (double)123);
 
     // Testing valid object
     struct aws_json_node *object_node = aws_json_object_get(root, "object");
     ASSERT_NOT_NULL(object_node);
-    ASSERT_TRUE(aws_json_is_object(object_node) == true);
+    ASSERT_INT_EQUALS(aws_json_is_object(object_node), AWS_OP_SUCCESS);
     struct aws_json_node *sub_object_node = aws_json_object_get(object_node, "a");
     ASSERT_NOT_NULL(sub_object_node);
-    ASSERT_TRUE(aws_json_is_string(sub_object_node));
+    ASSERT_INT_EQUALS(aws_json_is_string(sub_object_node), AWS_OP_SUCCESS);
     ASSERT_TRUE(strcmp(aws_json_string_get(sub_object_node), "b") == 0);
 
     // Testing invalid object
