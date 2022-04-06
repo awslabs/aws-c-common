776da44fe06a9e160ab37363c8fecd0d4917a880
Noah Beard <ncbeard@amazon.com>
2022-04-06 15:04:00 -0400

    Removed print statements from allocate and deallocate

--- source/allocator_sba.c
+++ source/allocator_sba.c
@@ -433,13 +433,11 @@ static void s_sba_free(struct small_block_allocator *sba, void *addr) {
 
 static void *s_sba_mem_acquire(struct aws_allocator *allocator, size_t size) {
     struct small_block_allocator *sba = allocator->impl;
-    fprintf(stdout, "Allocated Bytes: %zu\n", size);
     return s_sba_alloc(sba, size);
 }
 
 static void s_sba_mem_release(struct aws_allocator *allocator, void *ptr) {
     struct small_block_allocator *sba = allocator->impl;
-    fprintf(stdout, "Released Pointer: %p\n", ptr);
     s_sba_free(sba, ptr);
 }
 
