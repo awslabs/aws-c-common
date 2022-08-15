2ff6b14f95f1b9763952f459fa087cbe368ee92a
Dengke Tang <dengket@amazon.com>
2022-08-15 15:22:24 -0700

    Fall back for load target

--- cmake/aws-c-common-config.cmake
+++ cmake/aws-c-common-config.cmake
@@ -1,12 +1,12 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+include(AwsCrtLoadTarget)
+
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 
 if(WIN32 OR UNIX OR APPLE)
     find_package(Threads REQUIRED)
 endif()
 
-if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
-else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
-endif()
-
+aws_load_target_default(@PROJECT_NAME@)
