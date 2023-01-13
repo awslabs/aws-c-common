5e64b039356c72d17fcf1fb0bfc7637f8c2270f7
Bret Ambrose <bretambrose@gmail.com>
2023-01-13 13:34:06 -0800

    Always pull out value of errno right after sys call (#971)
    * Always pull out value of errno right after sys call; conditional on failed result is acceptable
    * Disclaimer comment on all errno references

--- source/logging.c
+++ source/logging.c
@@ -502,7 +502,8 @@ static int s_noalloc_stderr_logger_log(
 
     int write_result = AWS_OP_SUCCESS;
     if (fwrite(format_buffer, 1, format_data.amount_written, impl->file) < format_data.amount_written) {
-        aws_translate_and_raise_io_error(errno);
+        int errno_value = errno; /* Always cache errno before potential side-effect */
+        aws_translate_and_raise_io_error(errno_value);
         write_result = AWS_OP_ERR;
     }
 
