f06caba19241caa613553990012532c2733d12b5
Zhihui Xia <zhvxia@amazon.com>
2023-01-26 09:32:59 -0800

    clang format

--- source/encoding.c
+++ source/encoding.c
@@ -510,7 +510,7 @@ int aws_utf8_validator_update_with_callback(
 
         // Extra parse for customized validation callback.
         if (validation_fn && validator->remaining == 0) {
-            if(!validation_fn(validator->codepoint)){
+            if (!validation_fn(validator->codepoint)) {
                 return aws_raise_error(AWS_ERROR_INVALID_UTF8);
             }
         }
