23ff863975e602452aafbe87dd36b4e4e1581e45
Michael Graeb <graebm@amazon.com>
2023-03-07 14:30:57 -0800

    Fix c++ compiler warnings

--- include/aws/common/string.h
+++ include/aws/common/string.h
@@ -291,7 +291,7 @@ int aws_array_list_comparator_string(const void *a, const void *b);
         const size_t len;                                                                                              \
         const uint8_t bytes[sizeof(literal)];                                                                          \
     } name##_s = {NULL, sizeof(literal) - 1, literal};                                                                 \
-    static const struct aws_string *(name) = (struct aws_string *)(&name##_s)
+    static const struct aws_string *name = (struct aws_string *)(&name##_s)
 
 /*
  * A related macro that declares the string pointer without static, allowing it to be externed as a global constant
