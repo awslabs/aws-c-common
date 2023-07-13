3a5655b11cb8e6ff974e06294e62c85de8f04457
Jonathan M. Henson <henso@amazon.com>
2023-07-12 17:43:57 -0700

    Add more generic AVX feature detection as well as flags for if avx512 intrinsics are available.

--- cmake/AwsSIMD.cmake
+++ cmake/AwsSIMD.cmake
@@ -8,18 +8,29 @@ if (USE_CPU_EXTENSIONS)
     if (MSVC)
         check_c_compiler_flag("/arch:AVX2" HAVE_M_AVX2_FLAG)
         if (HAVE_M_AVX2_FLAG)
-            set(AVX2_CFLAGS "/arch:AVX2")
+            set(AVX_CFLAGS "/arch:AVX2")
         endif()
     else()
         check_c_compiler_flag(-mavx2 HAVE_M_AVX2_FLAG)
         if (HAVE_M_AVX2_FLAG)
-            set(AVX2_CFLAGS "-mavx -mavx2")
+            set(AVX_CFLAGS "-mavx -mavx2")
         endif()
     endif()
 
+    if (MSVC)
+        check_c_compiler_flag("/arch:AVX512" HAVE_M_AVX512_FLAG)
+        if (HAVE_M_AVX512_FLAG)
+            set(AVX_CFLAGS "/arch:AVX512")
+        endif()
+    else()
+        check_c_compiler_flag(-mavx512f HAVE_M_AVX512_FLAG)
+        if (HAVE_M_AVX512_FLAG)
+            set(AVX_CFLAGS "-mavx512f -msse4.2 -mvpclmulqdq -mpclmul")
+        endif()
+    endif()
 
     set(old_flags "${CMAKE_REQUIRED_FLAGS}")
-    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${AVX2_CFLAGS}")
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${AVX_CFLAGS}")
 
     check_c_source_compiles("
         #include <immintrin.h>
@@ -35,7 +46,22 @@ if (USE_CPU_EXTENSIONS)
             _mm256_permutevar8x32_epi32(vec, vec);
 
             return 0;
-        }"  HAVE_AVX2_INTRINSICS)
+        }"  AWS_HAVE_AVX2_INTRINSICS)
+
+    check_c_source_compiles("
+        #include <nmintrin.h>
+        int main() {
+            __m128i a = _mm_setzero_si128();
+            return 0;
+        }" AWS_HAVE_SSE42_INTRINSICS)
+
+    check_c_source_compiles("
+        #include <immintrin.h>
+
+        int main() {
+            __m512 a = _mm512_setzero_ps();
+            return 0;
+        }" AWS_HAVE_AVX512_INTRINSICS)
 
     check_c_source_compiles("
         #include <immintrin.h>
@@ -45,7 +71,7 @@ if (USE_CPU_EXTENSIONS)
             __m256i vec;
             memset(&vec, 0, sizeof(vec));
             return (int)_mm256_extract_epi64(vec, 2);
-        }" HAVE_MM256_EXTRACT_EPI64)
+        }" AWS_HAVE_MM256_EXTRACT_EPI64)
 
     set(CMAKE_REQUIRED_FLAGS "${old_flags}")
 endif() # USE_CPU_EXTENSIONS
@@ -59,16 +85,17 @@ endmacro(simd_add_definition_if)
 # Configure private preprocessor definitions for SIMD-related features
 # Does not set any processor feature codegen flags
 function(simd_add_definitions target)
-    simd_add_definition_if(${target} HAVE_AVX2_INTRINSICS)
-    simd_add_definition_if(${target} HAVE_MM256_EXTRACT_EPI64)
+    simd_add_definition_if(${target} AWS_HAVE_AVX2_INTRINSICS)
+    simd_add_definition_if(${target} AWS_HAVE_AVX512_INTRINSICS)
+    simd_add_definition_if(${target} AWS_HAVE_MM256_EXTRACT_EPI64)
 endfunction(simd_add_definitions)
 
-# Adds source files only if AVX2 is supported. These files will be built with
-# avx2 intrinsics enabled.
-# Usage: simd_add_source_avx2(target file1.c file2.c ...)
-function(simd_add_source_avx2 target)
+# Adds AVX flags, if any, that are supported. These files will be built with
+# available avx intrinsics enabled.
+# Usage: simd_add_source_avx(target file1.c file2.c ...)
+function(simd_add_source_avx target)
     foreach(file ${ARGN})
         target_sources(${target} PRIVATE ${file})
-        set_source_files_properties(${file} PROPERTIES COMPILE_FLAGS "${AVX2_CFLAGS}")
+        set_source_files_properties(${file} PROPERTIES COMPILE_FLAGS "${AVX_CFLAGS}")
     endforeach()
-endfunction(simd_add_source_avx2)
+endfunction(simd_add_source_avx)
