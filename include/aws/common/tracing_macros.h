95b594c832b1d138432fb2b672de6cadb5ba9564
Waqar Ahmed Khan <waahm7@gmail.com>
2023-08-09 11:44:33 -0700

    initial

---
+++ include/aws/common/tracing_macros.h
@@ -0,0 +1,17 @@
+#ifndef AWS_TRACING_MACROS_H
+#define AWS_TRACING_MACROS_H
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+/* clang-format off */
+
+#ifdef AWS_ENABLE_TRACING
+#include <ittnotify.h>
+#endif
+
+
+
+
+#endif /* AWS_TRACING_MACROS_H */
