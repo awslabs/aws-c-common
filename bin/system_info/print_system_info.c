6a7c30c38659f9ee18c635568a283352c51953a6
Jonathan M. Henson <henso@amazon.com>
2023-10-05 15:26:26 -0700

    fixed json output formatting.

--- bin/system_info/print_system_info.c
+++ bin/system_info/print_system_info.c
@@ -22,14 +22,14 @@ int main(void) {
     fprintf(stdout, "crt-detected env: {\n");
 
     struct aws_byte_cursor virtualization_vendor = aws_system_environment_get_virtualization_vendor(env);
-    fprintf(stdout, "  'virtualization vendor': '" PRInSTR "'\n", (int)virtualization_vendor.len, virtualization_vendor.ptr);
+    fprintf(stdout, "  'virtualization vendor': '" PRInSTR "',\n", (int)virtualization_vendor.len, virtualization_vendor.ptr);
     struct aws_byte_cursor product_name = aws_system_environment_get_virtualization_product_name(env);
-    fprintf(stdout, "  'product name': '" PRInSTR "'\n", (int)product_name.len, product_name.ptr);
-    fprintf(stdout, "  'number of processors': '%lu'\n", (unsigned long)aws_system_environment_get_processor_count(env));
+    fprintf(stdout, "  'product name': '" PRInSTR "',\n", (int)product_name.len, product_name.ptr);
+    fprintf(stdout, "  'number of processors': '%lu',\n", (unsigned long)aws_system_environment_get_processor_count(env));
     size_t numa_nodes = aws_system_environment_get_cpu_group_count(env);
 
     if (numa_nodes > 1) {
-        fprintf(stdout, "  'numa architecture': 'true'\n");
+        fprintf(stdout, "  'numa architecture': 'true',\n");
         fprintf(stdout, "  'number of numa nodes': '%lu'\n", (unsigned long)numa_nodes);
     } else {
         fprintf(stdout, "  'numa architecture': 'false'\n");
