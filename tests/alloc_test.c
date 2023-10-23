d958dcc274d8124779f9ce346dd7bedabed0febd
Dmitriy Musatkin <musatkd@amazon.com>
2023-10-23 09:49:53 -0700

    lint

--- tests/alloc_test.c
+++ tests/alloc_test.c
@@ -52,7 +52,8 @@ static int s_test_alloc_nothing_fn(struct aws_allocator *allocator, void *ctx) {
         .mem_acquire = s_test_alloc_acquire,
         .mem_release = s_test_alloc_release,
         .mem_realloc = s_test_realloc,
-        .mem_calloc = s_test_calloc};
+        .mem_calloc = s_test_calloc, 
+    };
 
     /* realloc should handle the case correctly, return null, and free the memory */
     void *p = aws_mem_acquire(&test_allocator, 12);
