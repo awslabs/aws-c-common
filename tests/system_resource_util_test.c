c956f114f7fed2d3c9841e6aeced5af2c913fab2
Dmitriy Musatkin <musatkd@amazon.com>
2023-10-27 11:06:29 -0700

    allocate a buf before

--- tests/system_resource_util_test.c
+++ tests/system_resource_util_test.c
@@ -3,18 +3,23 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+#include <aws/common/byte_buf.h>
 #include <aws/common/system_resource_util.h>
 
 #include <aws/testing/aws_test_harness.h>
 
 static int s_test_memory_usage_maxrss(struct aws_allocator *allocator, void *ctx) {
-    (void)allocator;
     (void)ctx;
+
+    struct aws_byte_buf temp;
+    aws_byte_buf_init(&temp, allocator, 8 * 1024 * 1024);
 
     struct aws_memory_usage_stats mu;
     ASSERT_SUCCESS(aws_init_memory_usage_for_current_process(&mu));
 
     ASSERT_TRUE(mu.maxrss > 0);
+
+    aws_byte_buf_clean_up(&temp);
 
     return 0;
 }
