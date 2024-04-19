80e65fbafaf2efed96f1beaecaa80c1f85df4282
Waqar Ahmed Khan <waahm7@gmail.com>
2024-04-19 13:05:53 -0700

    host utils

--- source/host_utils.c
+++ source/host_utils.c
@@ -2,6 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+#include <aws/common/host_utils.h>
 #include <aws/common/string.h>
 #include <inttypes.h>
 
