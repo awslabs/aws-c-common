0207e3044bb91d9966a71cfe6369627569db6597
DmitriyMusatkin <musatkd@amazon.com>
2024-07-01 16:48:36 -0700

    typo

--- cmake/AwsSanitizers.cmake
+++ cmake/AwsSanitizers.cmake
@@ -71,7 +71,6 @@ function(aws_add_sanitizers target)
             #target_link_libraries(${PROJECT_NAME} PUBLIC
             #    $<$<CONFIG:Release>:clang_rt.asan_dynamic-x86_64>
             #    $<$<CONFIG:Release>:clang_rt.asan_dynamic_runtime_thunk-x86_64>
-        )
         else()
             target_compile_options(${target} PRIVATE -fno-omit-frame-pointer -fsanitize=${PRESENT_SANITIZERS})
             target_link_libraries(${target} PUBLIC "-fno-omit-frame-pointer -fsanitize=${PRESENT_SANITIZERS}")
