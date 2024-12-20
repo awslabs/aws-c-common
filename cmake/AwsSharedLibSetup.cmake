c720814e48a99370c3c1a2928bb82d9ce66c9f05
Michael Graeb <graebm@amazon.com>
2024-12-19 16:15:54 -0800

    respect custom cmake install path for bin/lib/include

--- cmake/AwsSharedLibSetup.cmake
+++ cmake/AwsSharedLibSetup.cmake
@@ -1,21 +1,17 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 
-set(LIBRARY_DIRECTORY lib)
-set(RUNTIME_DIRECTORY bin)
-# Set the default lib installation path on GNU systems with GNUInstallDirs
-if (UNIX AND NOT APPLE)
-    include(GNUInstallDirs)
-    set(LIBRARY_DIRECTORY ${CMAKE_INSTALL_LIBDIR})
-    set(RUNTIME_DIRECTORY ${CMAKE_INSTALL_BINDIR})
+# NOTE: GNUInstallDirs is also fine for Windows and Mac. It sets reasonable defaults like "lib" and "bin"
+include(GNUInstallDirs)
+set(LIBRARY_DIRECTORY ${CMAKE_INSTALL_LIBDIR})
+set(RUNTIME_DIRECTORY ${CMAKE_INSTALL_BINDIR})
+set(INCLUDE_DIRECTORY ${CMAKE_INSTALL_INCLUDEDIR})
 
-    # this is the absolute dumbest thing in the world, but find_package won't work without it
-    # also I verified this is correctly NOT "lib64" when CMAKE_C_FLAGS includes "-m32"
-    if (${LIBRARY_DIRECTORY} STREQUAL "lib64")
-        set(FIND_LIBRARY_USE_LIB64_PATHS true)
-    endif()
+# this is the absolute dumbest thing in the world, but find_package won't work without it
+# also I verified this is correctly NOT "lib64" when CMAKE_C_FLAGS includes "-m32"
+if (${LIBRARY_DIRECTORY} STREQUAL "lib64")
+    set(FIND_LIBRARY_USE_LIB64_PATHS true)
 endif()
-
 
 function(aws_prepare_shared_lib_exports target)
     if (BUILD_SHARED_LIBS)
