0827c93c2d18aacbef2cf40834c4a8b36c8c54e0
Dengke Tang <dengket@amazon.com>
2025-08-19 16:55:50 -0700

    why freebsd crashes?

--- tests/file_test.c
+++ tests/file_test.c
@@ -647,6 +647,11 @@ static int s_test_file_path_read_from_offset_direct_io(struct aws_allocator *all
     }
 
     aws_byte_buf_clean_up(&output_buf);
+#    ifdef __FreeBSD__
+
+    /* Cleanup */
+    s_file_path_read_from_offset_tester_cleanup(&tester);
+#    else
 
     /* Test 3: Test unaligned offset - should fail with AWS_ERROR_INVALID_ARGUMENT */
     ASSERT_SUCCESS(aws_byte_buf_init(&output_buf, tester.aligned_allocator, page_size));
@@ -670,6 +675,7 @@ static int s_test_file_path_read_from_offset_direct_io(struct aws_allocator *all
 
     /* Cleanup */
     s_file_path_read_from_offset_tester_cleanup(&tester);
+#    endif
 #else
     struct aws_string *file_path =
         aws_string_new_from_c_str(allocator, "test_file_path_read_from_offset_direct_io.txt");
