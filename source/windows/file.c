3a5e638aee99c9f1d65696f7df8c4e5d3dc5805c
Dengke Tang <dengket@amazon.com>
2026-07-21 10:03:28 -0700

    aws_file_get_last_modified_epoch (#1258)

--- source/windows/file.c
+++ source/windows/file.c
@@ -544,3 +544,43 @@ int aws_file_get_length(FILE *file, int64_t *length) {
 
     return AWS_OP_SUCCESS;
 }
+
+int aws_file_get_last_modified_epoch(FILE *file, uint64_t *last_modified_ns) {
+    if (file == NULL) {
+        return aws_raise_error(AWS_ERROR_INVALID_FILE_HANDLE);
+    }
+
+    int fd = _fileno(file);
+    if (fd == -1) {
+        return aws_raise_error(AWS_ERROR_INVALID_FILE_HANDLE);
+    }
+
+    HANDLE os_file = (HANDLE)_get_osfhandle(fd);
+    if (os_file == INVALID_HANDLE_VALUE) {
+        int errno_value = errno; /* Always cache errno before potential side-effect */
+        return aws_translate_and_raise_io_error(errno_value);
+    }
+
+    FILETIME last_write_time;
+    if (!GetFileTime(os_file, NULL /*creation*/, NULL /*access*/, &last_write_time)) {
+        return aws_raise_error(AWS_ERROR_SYS_CALL_FAILURE);
+    }
+
+    /*
+     * FILETIME is 100-nanosecond intervals since January 1, 1601 UTC.
+     * Convert to nanoseconds since unix epoch (January 1, 1970 UTC).
+     * The difference is 11644473600 seconds = 116444736000000000 hundred-nanos.
+     * See aws_sys_clock_get_ticks() in clock.c for the same conversion.
+     */
+    ULARGE_INTEGER int_conv;
+    int_conv.LowPart = last_write_time.dwLowDateTime;
+    int_conv.HighPart = last_write_time.dwHighDateTime;
+
+    static const uint64_t WINDOWS_TICK = 10000000;
+    static const uint64_t EC_TO_UNIX_EPOCH = 11644473600LL;
+    static const uint64_t FILE_TIME_TO_NS = 100;
+
+    *last_modified_ns = (int_conv.QuadPart - (WINDOWS_TICK * EC_TO_UNIX_EPOCH)) * FILE_TIME_TO_NS;
+
+    return AWS_OP_SUCCESS;
+}
